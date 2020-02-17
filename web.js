147bc7a1e10dd90c80df8755d8cfe8d5d66ff198
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-02-17 12:34:07 +0700

    Initial commit

---
+++ web.js
@@ -0,0 +1,34 @@
+function run()
+{
+  let a=console.log;
+  console.log =  function(val)
+  {
+    let str;
+    switch(typeof val)
+    {
+      case "string":
+        str='"'+val+'"';
+        break;
+      case "object":
+        str="Object:  "+JSON.stringify(val);
+        break;
+      default:
+        str=(val||"undefined").toString();
+    }
+    document.getElementById("tester-output").value+=str+"\n";
+  }
+  let output=document.getElementById("tester-output");
+  output.removeAttribute("style");
+  output.value="";
+  try
+  {
+    eval(document.getElementById("tester").value);
+  }
+  catch(e)
+  {
+    output.setAttribute("style","color:red");
+    output.value=e.name+": "+e.message;
+  }
+  console.log=a;
+}
+document.getElementById("run").addEventListener("click",run);
