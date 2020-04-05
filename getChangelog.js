e3771585dcc975c884315661935be67a291db729
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-04-05 08:43:39 +0700

    Update getChangelog.js

--- getChangelog.js
+++ getChangelog.js
@@ -1,4 +1,4 @@
 let src=document.createElement("script");
 //src.src="https://bhpsngum.github.io/getChangelog.php";
-src.innerHTML='$.ajax({url:"https://starblast.io/changelog.txt",success: function(data){console.log(data)},crossDomain:true,dataType: 'jsonp',processData:false,error: function(e){console.log(e)}});';
+src.innerHTML='$.ajax({url:"https://starblast.io/changelog.txt",success: function(data){console.log(data)},crossDomain:true,dataType: "jsonp",processData:false,error: function(e){console.log(e)}});';
 document.body.appendChild(src);
