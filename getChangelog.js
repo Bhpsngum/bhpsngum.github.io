4ee3caea46e864b3e43c569677229bcc2742f6b1
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-04-05 08:42:21 +0700

    Update getChangelog.js

--- getChangelog.js
+++ getChangelog.js
@@ -1,4 +1,4 @@
 let src=document.createElement("script");
 //src.src="https://bhpsngum.github.io/getChangelog.php";
-src.innerHTML="console.log('ok')";
+src.innerHTML='$.ajax({url:"https://bhpsngum.github.io/getChangelog.js",success: function(data){console.log(data)},crossDomain:true,dataType: 'jsonp',processData:false,error: function(e){console.log(e)}});';
 document.body.appendChild(src);
