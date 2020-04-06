00792b66cf4376650ee2529e7fbafe045e7d1f3c
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-04-06 20:56:03 +0700

    Delete getChangelog.js

--- getChangelog.js
+++
@@ -1,4 +0,0 @@
-let src=document.createElement("script");
-//src.src="https://bhpsngum.github.io/getChangelog.php";
-src.innerHTML='$.ajax({url:"https://starblast.io/changelog.txt",success: function(data){console.log(data)},crossDomain:true,dataType: "text",error: function(e){console.log(e)}});';
-document.body.appendChild(src);
