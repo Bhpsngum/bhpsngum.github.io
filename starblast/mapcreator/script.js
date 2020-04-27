fcdbd0e5e5e8f2413b7eec7b95ed656e276173d2
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-04-27 23:09:02 +0700

    reverting to previous versions

--- starblast/mapcreator/script.js
+++ starblast/mapcreator/script.js
@@ -35,10 +35,10 @@ function changeASSize(num)
 function modifyMap()
 {
   let d=[];
-  for (let i=0;i<Number(localStorage.size)||20;i++)
+  for (let i=0;i<localStorage.size;i++)
   {
     d.push([]);
-    for (let j=0;j<Number(localStorage.size)||20;j++) d[i].push(Math.round(Number($(`#p${i}-${j} >img`).width())/3));
+    for (let j=0;j<Number(localStorage.size)||0;j++) d[i].push(Math.round(Number($(`#p${i}-${j} >img`).width())/3));
   }
   localStorage.setItem("array",JSON.stringify(d));
 }
@@ -76,13 +76,14 @@ function loadMap(data)
     if (d>200) d=200;
     else if (d<20) d=20;
     $("#map_size").val(d);
-    changeMap(d);
+    localStorage.setItem("size",d);
+    changeMap(Number(localStorage.size));
     for (let i=0;i<d;i++)
       for (let j=0;j<d;j++)
       {
-        let size=h[i][j]||0;
-        $(`#p${i}-${j} > img`).width(Math.round(size)*3);
-        $(`#p${i}-${j} > img`).height(Math.round(size)*3);
+        let check=h[i][j]||0;
+        $(`#p${i}-${j} > img`).width(Math.round(h[i][j])*3);
+        $(`#p${i}-${j} > img`).height(Math.round(h[i][j])*3);
       }
   }
   else check=false;
@@ -91,7 +92,7 @@ function loadMap(data)
 }
 function changeMap(data,tf)
 {
-  let size=data;
+  let size=isNaN(Number(data))?20:Number(data);
   if (size>200) size=200;
   else if (size<20) size=20;
   mapSize.val(size);
@@ -173,11 +174,11 @@ for (let i=1;i<=9;i++) cas+=`<td id='asc${i}' onclick = 'changeASSize(${i});this
 $("#asChoose").html(cas+"</tr>");
 if (!isNaN(Number(localStorage.as_size)) && Number(localStorage.as_size))
 document.querySelector("#asc"+Number(localStorage.as_size)).style= "border: 3px solid rgb(102, 102, 102)";
-changeMap(Number(localStorage.size)||20,1);
+changeMap(localStorage.size||20,1);
 loadMap();
 mapSize.on("change",function(){changeMap(mapSize.val())});
 $("#clearMap").on("click",function(){
-  changeMap(Number(localStorage.size)||20);
+  changeMap(localStorage.size);
 });
 $("#brush_size").on("change", function() {
   let size=$("#brush_size").val(),max=Number(localStorage.size)||20;
