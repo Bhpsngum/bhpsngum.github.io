236059a2e642fcc08ef3130059f5f8a01caa677d
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-07-28 17:25:16 +0700

    beta testing

--- starblast/mapeditor/beta/script.js
+++ starblast/mapeditor/beta/script.js
@@ -1,4 +1,7 @@
 (function(){
+  console.log('%c Stop!!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38)');
+  console.log('%cYou are accessing the Web Developing Area.\n\nPlease do not write/copy/paste/run any scripts here (unless you know what you\'re doing) to better protect yourself from loosing your map data, and even your other sensitive data.\n\nWe will not be responsible for any problems if you do not follow the warnings.', 'font-weight: bold; font-size: 15px;color: grey;');
+  console.log('%cMap Creator, made by Bhpsngum,\n\nfeel free to distribute the code and make sure to credit my name in your work if you intend to do that\n\nGitHub: https://github.com/Bhpsngum', 'font-weight: bold; font-size: 15px;color: Black;');
   var StarblastMap = {
     map: $("#map"),
     sizeInput: $("#map_size"),
