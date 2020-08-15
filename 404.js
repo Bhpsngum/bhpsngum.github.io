61060e55e46fcc171292b0fffab154d95b866c92
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-15 21:32:50 +0700

    lol why

--- 404.js
+++ 404.js
@@ -9,8 +9,8 @@
     [null]
   ]
   let i = random(name.length), ind = random(styles[i].length);
-  document.body.setAttribute("style", "color:"+(styles[i][ind]||"rgb(102,102,102)"));
-  document.querySelector("#message").innerHTML= `Here is a picture of ${name[i]} instead.`;
+  window.document.body.setAttribute("style", "color:"+(styles[i][ind]||"rgb(102,102,102)"));
+  window.document.querySelector("#message").innerHTML= `Here is a picture of ${name[i]} instead.`;
   let img_link = `https://bhpsngum.github.io/404_img/${name[i].replace(/\s/g,"_")}/${ind}.`;
   function testImage(i)
   {
@@ -18,8 +18,8 @@
     let u = new Image(),lnk = img_link+img_types[i];
     u.src = lnk;
     img.onload = function(){
-      document.body.setAttribute("style",document.body.getAttribute("style")+`background-image:url(${lnk});`);
-      document.querySelector("#link").href = lnk
+      window.document.body.setAttribute("style",window.document.body.getAttribute("style")+`background-image:url(${lnk});`);
+      window.document.querySelector("#link").href = lnk;
     }
     img.onerror = function(){testImage(i-1)}
   }
