63a5eeadab6fcb61972665bdf56675373cb1094b
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-15 21:37:33 +0700

    hmm

--- 404.js
+++ 404.js
@@ -8,20 +8,21 @@
     [null],
     [null]
   ]
-  let i = random(name.length), ind = random(styles[i].length), body = window.document.querySelector("#body");
-  body.setAttribute("style", "color:"+(styles[i][ind]||"rgb(102,102,102)"));
-  window.document.querySelector("#message").innerHTML= `Here is a picture of ${name[i]} instead.`;
-  let img_link = `https://bhpsngum.github.io/404_img/${name[i].replace(/\s/g,"_")}/${ind}.`;
-  function testImage(i)
-  {
-    if (i<0) return;
-    let u = new Image(),lnk = img_link+img_types[i];
-    u.src = lnk;
-    img.onload = function(){
-    body.setAttribute("style",body.getAttribute("style")+`background-image:url(${lnk});`);
-      window.document.querySelector("#link").href = lnk;
-    }
-    img.onerror = function(){testImage(i-1)}
-  }
-  testImage(img_types.length-1);
+  // let i = random(name.length), ind = random(styles[i].length), body = window.document.querySelector("#body");
+  // body.setAttribute("style", "color:"+(styles[i][ind]||"rgb(102,102,102)"));
+  // window.document.querySelector("#message").innerHTML= `Here is a picture of ${name[i]} instead.`;
+  // let img_link = `https://bhpsngum.github.io/404_img/${name[i].replace(/\s/g,"_")}/${ind}.`;
+  // function testImage(i)
+  // {
+  //   if (i<0) return;
+  //   let u = new Image(),lnk = img_link+img_types[i];
+  //   u.src = lnk;
+  //   img.onload = function(){
+  //   body.setAttribute("style",body.getAttribute("style")+`background-image:url(${lnk});`);
+  //     window.document.querySelector("#link").href = lnk;
+  //   }
+  //   img.onerror = function(){testImage(i-1)}
+  // }
+  // testImage(img_types.length-1);
+  console.log(document);
 })();
