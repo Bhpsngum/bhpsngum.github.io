ebef6fcc4311025ffcecdbc74f97b7a3e0ec6f0d
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-23 21:46:56 +0700

    ok lol

--- starblast/mods/script.js
+++ starblast/mods/script.js
@@ -1,11 +1,10 @@
 var ModInfo = function(data)
 {
-  return
-  `<div class="ModTab" id='${data.name||"unkonwn"}'>
+  return `<div class="ModTab" id='${data.name||"unkonwn"}'>
     <a href="${data.link.url}"><h4>${data.name}</h4></a>
     <h5 class="version">${data.version||""}</h5><br>
     <a href="${data.author.link||""}"><h6>${data.author.name}</h6></a>
-  </div>`
+  </div>`;
 }
 function loadError()
 {
