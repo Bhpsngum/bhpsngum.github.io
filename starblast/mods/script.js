b61056ae071c916cfe9b5ca8364f82749655d2da
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-24 09:23:45 +0700

    hmm

--- starblast/mods/script.js
+++ starblast/mods/script.js
@@ -2,12 +2,12 @@ var ModInfo = function(data)
 {
   var state = ["down","private","active"][data.link.state||0];
   this.html = `<div class="ModTab" id='${data.name||"unkonwn"}'>
-    <div style="float:left"><img src="${data.img||"img/default.png"}" height=100pt></div>
+    <div style="float:left"><img src="${data.img||"img/default.png"}"></div>
     <table>
-      <tr><td><h3><a class="${state}" title="The link is currently ${state}" href="${data.link.url}">${data.name}<sup>${data.version||""}</sup></a></h3></th></tr>
+      <tr><td><h3><a class="${state}" title="This link is currently ${state}" href="${data.link.url}">${data.name}<sup>${data.version||""}</sup></a></h3></th></tr>
+      <tr><td><h5>${data.author.map(data => `<a href="${data.link||""}">${(data.name||[]).join("/")}</a>`).join()}</h5></td></tr>
       ${(data.official)?"<tr title='This is an official mod'><td><p>Official mod</p></td></tr>":""}
       <tr><td><p><b>Game Mode(s): </b>${data.modes||"Unspecified"}</p></td></tr>
-      <tr><td><h5>${data.author.map(data => `<a href="${data.link||""}">${(data.name||[]).join("/")}</a>`).join()}</h5></td></tr>
       <tr><td><p>${data.description||"No description provided"}</p></td></tr>
     </table>
   </div>`;
