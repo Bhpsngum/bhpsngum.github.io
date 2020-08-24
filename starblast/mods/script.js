3d0f3156e766e6959e8c2e824902300740a7652a
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-24 08:46:34 +0700

    rebuild

--- starblast/mods/script.js
+++ starblast/mods/script.js
@@ -2,10 +2,11 @@ var ModInfo = function(data)
 {
   var state = ["down","private","active"][data.link.state||0];
   this.html = `<div class="ModTab" id='${data.name||"unkonwn"}'>
-    <div style="float:left"><img id="${data.name+"Img"}" src="default.png"></div>
+    <div style="float:left"><img id="${data.name+"Img"}" src="img/default.png"></div>
     <table>
       <tr><td><h3><a class="${state}" title="The link is currently ${state}" href="${data.link.url}">${data.name}<sup>${data.version||""}</sup></a></h3></th></tr>
       <tr><td><h5>${data.author.map(data => `<a href="${data.link||""}">${(data.name||[]).join("/")}</a>`).join()}</h5></td></tr>
+      <tr><td><p>${data.description}</p></td></tr>
     </table>
   </div>`;
 }
