c00c2b7186360f8175602fcc478c187a3f942cd1
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-24 20:26:35 +0700

    idk

--- starblast/mods/script.js
+++ starblast/mods/script.js
@@ -8,7 +8,7 @@
       <table>
         <tr><td><h3><a class="${state}" title="This link is currently ${state}" ${(data.link.url)?("href='"+data.link.url+"'"):""} target="_blank">${data.name}<sup>${data.version||""}</sup></a></h3></th></tr>
         <tr><td><h5>${data.author.map(data => `<a ${(data.link)?("href='"+data.link+"'"):""} target="_blank">${(data.name||[]).join("/")}</a>`).join()}</h5></td></tr>
-        ${(data.official)?("<tr "+(data.official<2)?"style='color:yellow'":""+" title='This "+(data.official<2)?"is currently":"used to be"+" an official mod in Modding Space'><td><p><i class='fa fa-fw fa-star'></i>Official mod</p></td></tr>"):""}
+        ${(data.official)?("<tr "+((data.official<2)?"style='color:yellow'":"")+" title='This "+((data.official<2)?"is currently":"used to be")+" an official mod in Modding Space'><td><p><i class='fa fa-fw fa-star'></i>Official mod</p></td></tr>"):""}
         <tr><td><p><b>Game Mode(s): </b>${data.modes||"Unspecified"}</p></td></tr>
         <tr><td><p>${data.description||"No description provided."}</p></td></tr>
       </table>
