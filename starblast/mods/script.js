ef3a0d49872df11d4be4f5da516df67300831d0c
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-24 08:13:30 +0700

    hmm

--- starblast/mods/script.js
+++ starblast/mods/script.js
@@ -1,8 +1,9 @@
 var ModInfo = function(data)
 {
+  var state = ["down","private","active"][data.link.state||0];
   return `<div class="ModTab" id='${data.name||"unkonwn"}'>
     <table>
-      <tr><td><h3><a href="${data.link.url}">${data.name}<sup>${data.version||""}</sup></a></h3></th></tr>
+      <tr><td><h3><a class="${state}" title="The link is currently ${state}" href="${data.link.url}">${data.name}<sup>${data.version||""}</sup></a></h3></th></tr>
       <tr><td><h5><a href="${data.author.link||""}">${data.author.name}</a></h5></td></tr>
     </table>
   </div>`;
