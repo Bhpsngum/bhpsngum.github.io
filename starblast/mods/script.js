0c938f37f9097ff56479954e2294d3deece43fcb
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-25 21:27:24 +0700

    hmmm done

--- starblast/mods/script.js
+++ starblast/mods/script.js
@@ -7,8 +7,8 @@
     this.html = `<div class="ModTab" id='${data.name||"unkonwn"}'>
       <div style="float:left"><img src="${data.img||"img/default.png"}"></div>
       <table>
-        <tr><td><h3><a class="${state}" title="This link is currently ${state}" ${(data.link.url)?("href='"+data.link.url+"'"):""} target="_blank">${(key.name)?data.name.replace(key.name,"g",function(v){return `<mn>${v}</mn>`}):data.name}<sup>${data.version||""}</sup></a></h3></th></tr>
-        <tr><td><h5>${data.author.map(data => `<a ${(data.link)?("href='"+data.link+"'"):""} target="_blank">${(data.name||[]).map(data => (key.author)?data.replace(key.author,"g",function(v){return `<ma>${v}</ma>`}):data).join("/")}</a>`).join()}</h5></td></tr>
+        <tr><td><h3><a class="${state}" title="This link is currently ${state}" ${(data.link.url)?("href='"+data.link.url+"'"):""} target="_blank">${(key.name)?data.name.replace(key.name,"gi",function(v){return `<mn>${v}</mn>`}):data.name}<sup>${data.version||""}</sup></a></h3></th></tr>
+        <tr><td><h5>${data.author.map(data => `<a ${(data.link)?("href='"+data.link+"'"):""} target="_blank">${(data.name||[]).map(data => (key.author)?data.replace(key.author,"gi",function(v){return `<ma>${v}</ma>`}):data).join("/")}</a>`).join()}</h5></td></tr>
         ${(data.official)?("<tr "+((data.official<2)?"style='color:yellow'":"")+" title='This "+((data.official<2)?"is currently":"used to be")+" an official mod in Modding Space'><td><p><i class='fa fa-fw fa-star'></i>Official mod</p></td></tr>"):""}
         <tr><td><p><b>Game Mode(s): </b>${data.modes||"Unspecified"}</p></td></tr>
         <tr><td><p>${data.description||"No description provided."}</p></td></tr>
