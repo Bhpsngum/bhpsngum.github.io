51fd97ffafd3d997abba32445395f460b87841e3
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-25 14:35:24 +0700

    hmm

--- nav.js
+++ nav.js
@@ -1,2 +1,6 @@
-var u = window.location.pathname,t;console.log(u);
-if (t = u != u.toLowerCase(),t) window.location.pathname = u.toLowerCase();
+var u = window.location.pathname,t;
+if (t = u != u.toLowerCase(),t)
+{
+  document.querySelector('title').innerHTML = "A sec...";
+  window.location.pathname = u.toLowerCase();
+}
