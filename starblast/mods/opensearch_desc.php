a7ead6fcb7d950efed8e4823300987445792c47f
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-08-27 00:00:25 +0700

    again

--- starblast/mods/opensearch_desc.php
+++ starblast/mods/opensearch_desc.php
@@ -2,7 +2,6 @@
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
   <ShortName>by Name: SB Mods</ShortName>
   <Description>Search archived mods by name</Description>
-  <Image height="16" width="16" type="image/x-icon">https://bhpsngum.github.io/starblast/mods/icon_light.png</Image>
   <Url type="text/html" method="get" template="https://bhpsngum.github.io/starblast/mods/?search&name={searchTerms}" />
   <moz:SearchForm>https://bhpsngum.github.io/starblast/mods/</moz:SearchForm>
 </OpenSearchDescription>
