8ac0c44e77ca643be7ed3c879e4967676bfa28fe
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-09-06 21:12:23 +0700

    beta testing

--- starblast/mapeditor/beta/script.js
+++ starblast/mapeditor/beta/script.js
@@ -277,11 +277,10 @@
         {
           c2d.clearRect(x*40+8,y*40+8,36,36);
           c2d.beginPath();
-          let t = [x*40+4+(40-num*3)/2,y*40+4+(40-num*3)/2,num*3,num*3];
           c2d.fillStyle = this.color;
-          c2d.drawImage(this.template,...t);
+          c2d.drawImage(this.template,x*40+4+(40-num*3)/2,y*40+4+(40-num*3)/2,num*3,num*3);
           c2d.globalCompositeOperation = "source-atop";
-          c2d.fillRect(...t);
+          c2d.fillRect(x*40+8,y*40+8,36,36);
           c2d.globalCompositeOperation = "source-over";
           if (num == 0) StarblastMap.pattern.delete(`${x}-${y}`);
           else StarblastMap.pattern.set(`${x}-${y}`,num);
