2526b3cfdeac388ae4e08143dac95705089f346f
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-09-30 19:00:19 +0700

    testing

--- starblast/mapeditor/beta/script.js
+++ starblast/mapeditor/beta/script.js
@@ -815,21 +815,19 @@
     }
   });
   let query=window.location.search.replace(/^\?/,"").split("="),error;
-  console.log(window.location.search);
   if (error = query[0] === "", !error)
   {
     switch (query[0].toLowerCase())
     {
       case "map":
-        if (error = confirm("Map pattern from URL detected!\nLoad map?\n(Note: this action cannot be undone)"), !error) {
-          let datamap = LZString.decompressFromEncodedURIComponent(query[1]).split("-").map(i => i.length);
-          if (error = !(datamap.length != Math.max(...datamap)), !error) {
-            if (error = !confirm("You are using the old permalink method.\nDo you want to go to the new one?"), !error) {
-              window.open("?map="+StarblastMap.export("url",StarblastMap.import("url-old",query[1],0,1)));
-              return;
-            }
-            else StarblastMap.import("url",query[1],1);
+        let datamap = LZString.decompressFromEncodedURIComponent(query[1]).split("-").map(i => i.length);
+        if (error = !(datamap.length != Math.max(...datamap)), !error) {
+          console.log(query[1]);
+          if (error = !confirm("You are using the old permalink method.\nDo you want to go to the new one?"), !error) {
+            window.open("?map="+StarblastMap.export("url",StarblastMap.import("url-old",query[1],0,1)));
+            return;
           }
+          else (error = confirm("Map pattern from URL detected!\nLoad map?\n(Note: this action cannot be undone)"), !error) && StarblastMap.import("url",query[1],1);
         }
         break;
       case "feedback":
