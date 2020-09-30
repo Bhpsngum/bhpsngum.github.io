cf31048a40e677e47350ac9a8d7c9f3add21c5ba
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-09-30 22:16:06 +0700

    m

--- starblast/mapeditor/beta/script.js
+++ starblast/mapeditor/beta/script.js
@@ -821,15 +821,14 @@
     {
       case "map":
         let datamap = LZString.decompressFromEncodedURIComponent(query[1]).split("-").map(i => i.length);
-        console.log(datamap);
         if (error = (datamap.length != Math.max(...datamap)), error) {
-          console.log(query[1]);
           if (error = !confirm("You are using the old permalink method.\nDo you want to go to the new one?"), !error) {
             window.open("?map="+StarblastMap.export("url",StarblastMap.import("url-old",query[1],0,1)));
             return;
           }
         }
         else (error = !confirm("Map pattern from URL detected!\nLoad map?\n(Note: this action cannot be undone)"), !error) && StarblastMap.import("url",query[1],1);
+        console.log(StarblastMap.import("url",query[1],0,1));
         break;
       case "feedback":
         $("title")[0].innerHTML = "Redirecting...";
