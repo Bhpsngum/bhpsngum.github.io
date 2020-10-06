b3265b7ef0295da9d442f86ef48810f740006563
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-10-06 15:20:17 +0700

    push

--- starblast/sscv/script.js
+++ starblast/sscv/script.js
@@ -2,12 +2,27 @@
   function showError() {
     alert("Cannot convert the requested code!");
   }
-  function convert() {
+  function convert(forced) {
+    let json = $("#jsonInput").val() || localStorage.getItem("coffee-input"), results;
     try {
-      let json = $("#1");
-      // hmm
+      eval("results=function(){return "+json.replace(/^(\s|\n|\r)+/,"").replace(/^(var|let|const)/,"").replace(/^\n+/,"")+"}();");
+      results = "return "+js2coffee.build("var model="+results).code.replace(/\n(\s+)'([^']+)':/g,"\n$1$2:").replace(/\[[^\]]+\]/g,function(v) {
+        return v.replace(/\n/g,"").replace(/\s+/g,",").replace(/,\]/g,"]").replace(/\[,/g,"[");
+      });
     }
-    catch(e){showError()};
+    catch(e){
+      if (forced) {
+        json = "(JSON) Modexport code"
+        results = "(CoffeeScript) Ship Editor code";
+      }
+      else {
+        showError();
+        return;
+      }
+    };
+    localStorage.setItem("coffee-input",json);
+    $("#coffeescriptOutput").val(results);
+    $("#jsonInput").val(json);
   }
-  $("#convert").on("click",convert);
+  $("#convert").on("click",function(){convert()});
 })();
