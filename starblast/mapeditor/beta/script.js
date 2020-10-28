dbdcab554f1a390a88dcfdf76e00a85d9e317910
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-10-28 14:22:44 +0700

    :thonk:

--- starblast/mapeditor/beta/script.js
+++ starblast/mapeditor/beta/script.js
@@ -1038,7 +1038,7 @@ t = (function(){
             "padding-top": (this.main.height()+5)+"px",
             "padding-bottom": $("#footer").height()+"px"
           });
-          $("#info").css("width",($(window).outerWidth()-$("#XY").width()-10)+"px")
+          $("#info").css("width",($("#footer").width()-$("#XY").width()-10)+"px")
         },
         set: function(index) {
           for (let i=0;i<this.modules.length;i++) {
