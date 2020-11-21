f17045d8097aafef4c0a114c468f2c2ce53048f2
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-11-21 20:56:51 +0700

    hmm

--- starblast/mods/stats/script.js
+++ starblast/mods/stats/script.js
@@ -1,12 +1,19 @@
 (function(){
-  tgf = [];
-  var modStatBox = function(stat, count, index) {
+  var mods = [], player_count = {}, timer = new Map(), init = !1, count_interval = 1000;
+  var modStatBox = function(stat, count, index, time) {
     let img = `<img id="img-${stat.mod_id}"src='https://starblast.data.neuronality.com/modding/img/${stat.mod_id != "none"?stat.mod_id:"prototypes"}.jpg'>`,
     statinfo = `<h3 style="text-align:center">${stat.title} <sup>${stat.version}</sup></h3>
       ${stat.new?"<b style='color:yellow;float:right'>NEW!</b>":""}
-      ${!stat.active?"<b style='color:red;float:right'>Removed</b>":""}
-      <b style='color:green'>${stat.featured?"Featuring in Modding Space":""}</b>
-      <p><b>Author:</b> ${stat.author}</p>
+      ${!stat.active?"<b style='color:red;float:right'>Removed</b>":""}`;
+    let t = Math.max(...[...timer.values()].filter(i => i != null && !isNaN(i))) === time;
+    if (stat.featured || t) {
+      statinfo+="<b style='color:green'>"
+      if (stat.featured) statinfo+="Featuring";
+      else statinfo+="Available";
+      statinfo+=" in Modding Space</b>";
+    }
+    else if (stat.active) statinfo += `<p>Next event: ${formatTime(time)}</p>`;
+    statinfo+=`<p><b>Author:</b> ${stat.author}</p>
       <p><b>Times played:</b> ${getNum(stat.timesplayed)}</p>
       ${(stat.active && count)?("<p>"+count+" playing</p>"):""}`, parent = $("#"+stat.mod_id), imgelement = $("#img-"+stat.mod_id), statelement = $("#stat-"+stat.mod_id);
     if (parent.length == 0) $('#modstats').append(`<div index = "${index}" class="modStatBox" id='${stat.mod_id}'>${img}<div id="stat-${stat.mod_id}">${statinfo}</div></div>`);
@@ -25,11 +32,23 @@
     ms = ms/1000;
     let t = Math.trunc(ms/3600), u = Math.trunc((ms-t*3600)/60);
     return [t,u,Math.trunc(ms-t*3600-u*60)].map(i => i<10?"0"+i.toString():i).join(":")
+  }, loadInfos = function() {
+    mods.forEach((mod, i) => modStatBox(mod, player_count[mod.mod_id]||0, i, timer.get(mod.mod_id)));
+    let elist = $("#modstats>*");
+    for (let i=0;i<elist.length-1;i++)
+      for (let j=i+1;j<elist.length;j++)
+        if (Number(elist[i].getAttribute("index")) > Number(elist[j].getAttribute("index"))) {
+          $(elist[j]).insertBefore($(elist[i]));
+          elist = $("#modstats>*");
+        }
+  }, count = function() {
+    for (let i of timer.keys()) timer.set(i, timer.get(i) - count_interval);
+    loadInfos();
   }, update = function() {
-    $.getJSON("https://starblast.io/modsinfo.json").then(function(mods) {
+    $.getJSON("https://starblast.io/modsinfo.json").then(function(modss) {
       $.getJSON("https://starblast.io/simstatus.json").then(function(players) {
-        mods = mods[0];
-        let player_count = {};
+        mods = modss[0];
+        player_count = {};
         for (let i of players)
           for (let j of i.systems) {
             if (j.mod_id && j.mode == "modding") {
@@ -40,7 +59,7 @@
         for (let i of mods) if (!i.featured && i.active) x+= 3600 * i.active_duration * 1000;
         let n = Date.now() % x, k = 0, w = 0, o = function(i, s) {
           var l, a, o, r, u, d, c, p, O;
-          return i.featured || (w += 3600 * i.active_duration * 1e3), a = n > k && n < w, O = Date.now() + k - n, k = w, O < Date.now() && (O += x), tgf[s]=O;
+          return i.featured || (w += 3600 * i.active_duration * 1e3), a = n > k && n < w, O = Date.now() + k - n, k = w, O < Date.now() && (O += x), timer.set(i.mod_id, O-Date.now());
         }
         mods.forEach((O,r) => O.active && o(O,r));
         mods.sort((a,b) => {
@@ -52,14 +71,12 @@
           let t = (player_count[a.mod_id] || 0) == 0, v = (player_count[b.mod_id] || 0) == 0;
           if ((t || v) && !(t&&v)) return (player_count[b.mod_id] || 0) - (player_count[a.mod_id] || 0);
           return (Number(b.date_created)||0) - (Number(a.date_created)||0);
-        }).forEach((mod, i) => modStatBox(mod, player_count[mod.mod_id]||0, i));
-        let elist = $("#modstats>*");
-        for (let i=0;i<elist.length-1;i++)
-          for (let j=i+1;j<elist.length;j++)
-            if (Number(elist[i].getAttribute("index")) > Number(elist[j].getAttribute("index"))) {
-              $(elist[j]).insertBefore($(elist[i]));
-              elist = $("#modstats>*");
-            }
+        });
+        loadInfos();
+        if (!init) {
+          setInterval(count, count_interval);
+          init = !0;
+        }
       });
     }).fail(e=>console.log(e));
   }
