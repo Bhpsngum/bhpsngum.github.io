4822c0fed08d700045c21dc4edce2cbb37828635
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-11-21 23:26:50 +0700

    bruh

--- starblast/mods/stats/script.js
+++ starblast/mods/stats/script.js
@@ -1,6 +1,11 @@
 (function(){
-  var mods = [], player_count = {}, timer = new Map(), init = !1, count_interval = 500;
-  var modStatBox = function(stat, count, index, time) {
+  var mods = [], player_count = {}, timer = new Map(), init = !1, count_interval = 500,
+  removed_time = {
+    "none": 1578454316626,
+    "prototypes": 1578454316626,
+    "racing": 1592486063588
+  },
+  modStatBox = function(stat, count, index, time) {
     let img = `<img ondragstart="return false;" id="img-${stat.mod_id}"src='https://starblast.data.neuronality.com/modding/img/${stat.mod_id != "none"?stat.mod_id:"prototypes"}.jpg'>`,
     statinfo = `<h3 style="text-align:center">${stat.title} <sup>${stat.version}</sup></h3>
       ${stat.new?"<b style='color:yellow;float:right'>NEW!</b>":""}
@@ -16,7 +21,7 @@
     }
     else if (stat.active) statinfo += `<p><b>Next event:</b> ${formatTime(time)}</p>`;
     statinfo+=`<p><b>Author:</b> ${stat.author}</p>
-      <p><b>Times played:</b> ${getNum(stat.timesplayed)} (${Math.round(stat.timesplayed/((Date.now()-stat.date_created)/1000/3600/24))} daily)</p>
+      <p><b>Times played:</b> ${getNum(stat.timesplayed)} (${Math.round((stat.date_removed||stat.timesplayed)/((Date.now()-stat.date_created)/1000/3600/24))} daily)</p>
       ${(stat.active && count)?("<p>"+count+" playing</p>"):""}`, parent = $("#"+stat.mod_id), imgelement = $("#img-"+stat.mod_id), statelement = $("#stat-"+stat.mod_id);
     if (parent.length == 0) $('#modstats').append(`<div index = "${index}" class="modStatBox" id='${stat.mod_id}'>${img}<div id="stat-${stat.mod_id}">${statinfo}</div></div>`);
     else {
@@ -33,7 +38,7 @@
   }, formatTime = function (ms) {
     ms = ms/1000;
     let t = Math.trunc(ms/3600), u = Math.trunc((ms-t*3600)/60);
-    return [t,u,Math.trunc(ms-t*3600-u*60)].map(i => i<10?"0"+i.toString():i).join(":")
+    return [t,u,Math.trunc(ms-t*3600-u*60)].map(i => Math.max(i,0)).map(i => i<10?"0"+i.toString():i).join(":")
   }, loadInfos = function() {
     mods.forEach((mod, i) => modStatBox(mod, player_count[mod.mod_id]||0, i, timer.get(mod.mod_id)));
     let elist = $("#modstats>*");
@@ -44,7 +49,14 @@
           elist = $("#modstats>*");
         }
   }, count = function() {
-    for (let i of timer.keys()) timer.set(i, timer.get(i) - count_interval);
+    var request_update = !1;
+    for (let i of timer.keys()) {
+      timer.set(i, timer.get(i) - count_interval);
+      if (timer.get(i) <= 0 && !request_update) {
+        update();
+        request_update = !0;
+      }
+    }
     loadInfos();
   }, update = function() {
     $.getJSON("https://starblast.io/modsinfo.json").then(function(modss) {
@@ -58,7 +70,10 @@
             }
           }
         let x = 0;
-        for (let i of mods) if (!i.featured && i.active) x+= 3600 * i.active_duration * 1000;
+        for (let i of mods) {
+          if (!i.featured && i.active) x+= 3600 * i.active_duration * 1000;
+          i.date_removed = removed_time[i.mod_id];
+        }
         let n = Date.now() % x, k = 0, w = 0, o = function(i, s) {
           if (!i.featured) w += 3600 * i.active_duration * 1e3;
           var O = k - n;
