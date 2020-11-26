1d76b2d534f932532d1c4aba17110b195dcdef2f
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-11-27 00:19:48 +0700

    test

--- starblast/mods/stats/script.js
+++ starblast/mods/stats/script.js
@@ -23,7 +23,8 @@
     statinfo+=`<p><b>Author:</b> ${stat.author}</p>
       <p><b>First released:</b> ${stat.date_created?new Date(stat.date_created).toLocaleDateString('en-US',{weekday:'long',year:'numeric',month:'long',day:'numeric'}):"Unknown"}</p>
       <p><b>Times played:</b> ${getNum(stat.timesplayed)} (${Math.round(stat.timesplayed/(((stat.date_removed||Date.now())-stat.date_created)/1000/3600/24))} daily)</p>
-      ${(stat.active && count)?("<p>"+count+" playing</p>"):""}`, parent = $("#"+stat.mod_id), imgelement = $("#img-"+stat.mod_id), statelement = $("#stat-"+stat.mod_id);
+      ${(stat.active && count)?("<p>"+count+" playing</p>"):""}`;
+    let parent = $("#"+stat.mod_id), imgelement = $("#img-"+stat.mod_id), statelement = $("#stat-"+stat.mod_id), player_stat = $("#"+stat.mod_id);
     if (parent.length == 0) $('#modstats').append(`<div index = "${index}" class="modStatBox" id='${stat.mod_id}'>${img}<div id="stat-${stat.mod_id}">${statinfo}</div></div>`);
     else {
       parent.attr("index",index);
