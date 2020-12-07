00ed5971ccd2e07a01d059707631e92760c6711c
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-07 07:37:34 +0700

    testing

--- starblast/mapeditor/init.js
+++ starblast/mapeditor/init.js
@@ -1,3 +1,8 @@
 window.viewinfo = function (title,text) {
   $("#info").html(`<strong>${title?title+": ":""}</strong>${text||""}`);
 }
+$.ajax("/starblast/mapeditor/changelog.txt").then(function(data){
+  data.replace(/\d+\.\d+\.\d+/, function(version) {
+    $("#latest").html("View latest version (" + version + ")");
+  });
+}).fail(e => {});
