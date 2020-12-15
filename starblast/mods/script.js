7704b812f35f60337f4f5ffb9987925862e36bb9
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-16 02:26:22 +0700

    done

--- starblast/mods/script.js
+++ starblast/mods/script.js
@@ -83,7 +83,8 @@
     {
       try {
         mods.forEach(function(mod) {
-          for (let i in mod) (typeof mod[i] == "string") && mod[i] = encodeHTML(mod[i]);
+          for (let i in mod)
+            if (typeof mod[i] == "string") mod[i] = encodeHTML(mod[i]);
         });
         modsinfo = mods;
         localStorage.setItem("modsinfo",JSON.stringify(mods));
