c3ea840d431b41009687eea9a28b9ca6f6360daf
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-21 23:26:54 +0700

    hmm

--- redirect/redirect.js
+++ redirect/redirect.js
@@ -1,4 +1,5 @@
 $.getJSON("rd_list.json").then(function(data){
-  let d,id=location.search.replace(/^\?id\=/,()=>(d=!0,""));
-  $('html').html('<head><title>Redirecting...</title></head><body style="font-family:Verdana">'+((!d && data.map(i=>i.id).indexOf(id)==-1)?"We couldn't find the link you were looking for.<br><a href='/'>Back to the main page</a>":`Redirecting, please wait...<br> Click <a href="${data.filter(i=>i.id==id)[0].url}">here</a> if your browser does not redirect you automatically.`)+'</body>');
+  let d,id=location.search.replace(/^\?id\=/,()=>(d=!0,"")),t=!d && data.map(i=>i.id).indexOf(id)==-1,g(data.filter(i=>i.id==id)[0]||{}).url;
+  $('html').html('<head><title>Redirecting...</title></head><body style="font-family:Verdana">'+(t?"We couldn't find the link you were looking for.<br><a href='/'>Back to the main page</a>":`Redirecting, please wait...<br> Click <a href="${g}">here</a> if your browser does not redirect you automatically.`)+'</body>');
+  t && window.open(g,'_self');
 });
