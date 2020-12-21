5b6184df483f2d764e0d98f8dd1d7ece9b1b1438
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-21 23:41:08 +0700

    testing again

--- redirect/redirect.js
+++ redirect/redirect.js
@@ -1,5 +1,6 @@
 $.getJSON("rd_list.json").then(function(data){
-  let d,id=location.search.replace(/^\?id\=/,()=>(d=!0,"")),t=!d&&data.map(i=>i.id).indexOf(id)==-1,g=(data.filter(i=>i.id==id)[0]||{}).url;
-  $('html').html('<head><title>Redirecting...</title><link rel="icon" href="favicon.ico"></link></head><body style="font-family:Verdana">'+(t?"We couldn't find the link you were looking for.<br><a href='/'>Back to the main page</a>":`Redirecting, please wait...<br> Click <a href="${g}">here</a> if your browser does not redirect you automatically.`)+'</body>');
+  let d,id=location.search.replace(/^\?id\=/,()=>(d=!0,"")),t=!d&&data.map(i=>i.id).indexOf(id)==-1,g=((data.filter(i=>i.id==id)[0]||{}).url||"").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"&apos;").replace(/"/g,"&quot;");
+  $('head').html('<title>Redirecting...</title><link rel="icon" href="favicon.ico">');
+  $('body').html('<body style="font-family:Verdana">'+(t?"We couldn't find the link you were looking for.<br><a href='/'>Back to the main page</a>":`Redirecting, please wait...<br> Click <a href="${g}">here</a> if your browser does not redirect you automatically.`)+'</body>');
   !t && window.open(g,'_self');
 });
