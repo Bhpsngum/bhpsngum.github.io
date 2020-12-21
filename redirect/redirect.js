15213d580afd71a85bd88e6109c3437ef1a77d53
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2020-12-22 00:07:00 +0700

    hmm

--- redirect/redirect.js
+++ redirect/redirect.js
@@ -2,6 +2,7 @@ $.getJSON("rd_list.json").then(function(data){
   let d,id=location.search.replace(/^\?id\=/,()=>(d=!0,"")),t=!d&&data.map(i=>i.id).indexOf(id)==-1,g=((data.filter(i=>i.id==id)[0]||{}).url||"").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"&apos;").replace(/"/g,"&quot;");
   $('head').html(`<title>${t?"Oops!":"Redirecting..."}</title><link rel="icon" href="favicon.ico">`);
   $('body').html('<body style="font-family:Verdana">'+(t?"We couldn't find the link you were looking for.<br><a href='/'>Back to the main page</a>":`Redirecting, please wait...<br> Click <a href="${g}">here</a> if your browser does not redirect you automatically.`)+'</body>');
+  console.log(t,data,id);
   delete window.$;
   delete window.jQuery;
   !t && window.open(g,'_self');
