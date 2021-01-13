8c08f5bc155b9366a89da8afa60146b0f84aa4c7
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-01-13 13:45:43 +0700

    next

--- starblast/mods/stats/script.js
+++ starblast/mods/stats/script.js
@@ -36,14 +36,14 @@
       let u = [];
       for (let i in (player_count_region[stat.mod_id]||{})) u.push(i);
       if (u.length > 0 && stat.active && player_count[stat.mod_id]) {
-        let evt = "<tr id='total_players-"+stat.mod_id+"'>", totalplayers = "<td><b>Current players</b</td><td>:</td><td>"+player_count[stat.mod_id]+"</td>", playerstat = u.filter(i => player_count_region[stat.mod_id][i]).map(i => `<tr ${player_stat.attr("hide") == "true"?"hidden":""}><td>${i}</td><td>:</td><td>${player_count_region[stat.mod_id][i]}</td></tr>`).join("");
-        if (player_stat.length == 0) $(`<table class="playerstat" hide="true" onclick='let e = $(this), t = e.attr("hide") == "true";$("#players-${stat.mod_id}>tbody>tr:not(#total_players-${stat.mod_id})").prop("hidden",!t);e.attr("hide",!t)' style='cursor:pointer;' id="players-${stat.mod_id}">${evt}${totalplayers}</tr>${playerstat}</table>`).insertAfter("#stat-"+stat.mod_id);
+        let evt = "<tr id='total_players-"+stat.mod_id+"'>", totalplayers = "<td><b>Current players</b</td><td>:</td><td>"+player_count[stat.mod_id]+"</td>", getStat = function(t){return u.filter(i => player_count_region[stat.mod_id][i]).map(i => `<tr ${$("#players-"+stat.mod_id).attr("hide") == "true"||t?"hidden":""}><td>${i}</td><td>:</td><td>${player_count_region[stat.mod_id][i]}</td></tr>`).join("")};
+        if (player_stat.length == 0) $(`<table class="playerstat" hide="true" onclick='let e = $(this), t = e.attr("hide") == "true";$("#players-${stat.mod_id}>tbody>tr:not(#total_players-${stat.mod_id})").prop("hidden",!t);e.attr("hide",!t)' style='cursor:pointer;' id="players-${stat.mod_id}">${evt}${totalplayers}</tr>${getStat(!0)}</table>`).insertAfter("#stat-"+stat.mod_id);
         else {
           let total_players = $("#total_players-"+stat.mod_id);
           if (total_players.length === 0) $("#players-"+stat.mod_id).prepend(evt+totalplayers+"</tr>");
           else total_players.html(totalplayers);
           $("#players-"+stat.mod_id+">tbody>tr:not(#total_players-"+stat.mod_id+")").remove();
-          $(playerstat).insertAfter("#total_players-"+stat.mod_id);
+          $(getStat()).insertAfter("#total_players-"+stat.mod_id);
         }
       }
       else player_stat.remove();
