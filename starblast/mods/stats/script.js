b15178caa0ce90132fc7b7873a84998c168fc2e6
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-01-13 12:09:42 +0700

    btw let's see

--- starblast/mods/stats/script.js
+++ starblast/mods/stats/script.js
@@ -11,7 +11,7 @@
     "battleroyale": 1511530440000
   },
   modStatBox = function(stat, count, index, time) {
-    let img = `<img id="img-${stat.mod_id}"src='https://starblast.data.neuronality.com/modding/img/${stat.mod_id != "none"?stat.mod_id:"prototypes"}.jpg' onerror="this.src = this.src">`,
+    let img = `<img id="img-${stat.mod_id}"src='https://starblast.data.neuronality.com/modding/img/${stat.mod_id != "none"?stat.mod_id:"prototypes"}.jpg' onerror="setTimeout(function(){this.src = this.src}.bind(this),5000)">`,
     statinfo = `<h3 style="text-align:center">${stat.title} <sup>${stat.version}</sup></h3>${stat.new?'<b style="color:yellow;float:right">NEW!</b>':""}${!stat.active?'<b style="color:red;float:right">Removed</b>':""}`;
     if (stat.featured || stat.open) {
       statinfo+="<a href='https://starblast.io/' style='text-decoration: none'><b style='color:green'>"
