3f81e7aecb982440a88d2b35406b38c887cac99f
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-01-15 13:21:25 +0700

    hmm

--- starblast/mods/stats/script.js
+++ starblast/mods/stats/script.js
@@ -170,6 +170,7 @@
   }, img_size = 360, padding_ratio = 1/30, full_ratio = 1+4*padding_ratio, adjustwidth = function(){
     let g = $(window).width(), x = Math.round(g/(img_size*full_ratio)), t = g/(x||1)/full_ratio, m = Math.trunc(t*padding_ratio);
     $(".modStatBox").css({width: Math.trunc(t)+"px",padding: m+"px", margin: m+"px","border-radius":m+"px"});
+    $(".playerstat").css({"font-size":$(".modStatBox").css("font-size")+"px"});
   }
   new ResizeSensor($("#title")[0], adjustwidth);
   update();
