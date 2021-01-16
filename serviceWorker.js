bf9b8607709bdfe087d628de31a4376613b045a1
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-01-16 23:08:58 +0700

    auto update sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
 window.addServiceWorker = function(handler) {
-  try{navigator.serviceWorker.register('/sw.js').then(handler)}
+  try{navigator.serviceWorker.register('/sw.js').then(function(v){v.update().then(function(t){typeof handler == "function" && handler(t)})})}
   catch(e){}
 }
