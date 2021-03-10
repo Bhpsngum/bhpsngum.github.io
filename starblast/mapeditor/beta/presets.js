c9d105dc340378641a49ac715b2e84ca4fd0535d
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-03-10 23:17:59 +0700

    test 2

--- starblast/mapeditor/beta/presets.js
+++ starblast/mapeditor/beta/presets.js
@@ -3,6 +3,7 @@ if (window.t) {
   window.open(t,"_self");
 }
 else (function(){
+  addServiceWorker();
   var links = [
     ["version",'/starblast/mapeditor/changelog.html','_blank'],
     ["feedback",'/redirect?id=MapEditorFeedback','_blank'],
