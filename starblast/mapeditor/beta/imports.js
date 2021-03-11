6b7b716a884e7e23fefedb1dbcaaa71354f25170
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-03-11 14:16:57 +0700

    bruh

--- starblast/mapeditor/beta/imports.js
+++ starblast/mapeditor/beta/imports.js
@@ -1,1 +1,18 @@
-window.imports = [...[...document.querySelectorAll("link")].map(i=>i.href),...[...document.querySelectorAll("script")].map(i=>i.src)].filter(i => i);
+var imports = [
+	"https://bhpsngum.github.io/starblast/mapeditor/icon_dark.png",
+  "https://bhpsngum.github.io/starblast/mapeditor/icon_light.png",
+  "https://bhpsngum.github.io/starblast/mapeditor/Asteroid.png",
+	"https://bhpsngum.github.io/starblast/mapeditor/beta/style.css",
+	"https://bhpsngum.github.io/starblast/mapeditor/beta/mapeditor.json",
+	"https://bhpsngum.github.io/serviceWorker.js",
+	"https://cdn.jsdelivr.net/gh/jquery/jquery/dist/jquery.min.js",
+	"https://cdn.jsdelivr.net/gh/marcj/css-element-queries/src/ResizeSensor.min.js",
+	"https://cdn.jsdelivr.net/gh/pieroxy/lz-string/libs/lz-string.min.js",
+	"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js",
+	"https://kit.fontawesome.com/ccd821e6cb.js",
+	"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-javascript.js",
+	"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/theme-monokai.js",
+	"https://cdn.jsdelivr.net/gh/tombigel/detect-zoom/detect-zoom.min.js",
+	"https://bhpsngum.github.io/starblast/mapeditor/beta/script.js",
+	"https://bhpsngum.github.io/starblast/mapeditor/beta/presets.js"
+];
