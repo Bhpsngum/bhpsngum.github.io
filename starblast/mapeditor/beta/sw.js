5ed36737ca41bcf4019c70dbd92d51bfb708651a
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-03-11 14:35:12 +0700

    bruh

--- starblast/mapeditor/beta/sw.js
+++ starblast/mapeditor/beta/sw.js
@@ -1,5 +1,5 @@
 self.importScripts('imports.js');
-const cacheName = "MapEditor-v3.1.5_beta";
+const cacheName = "MapEditorCaches";
 self.addEventListener('install', (e) => {
   e.waitUntil((async () => {
     const cache = await caches.open(cacheName);
@@ -7,13 +7,13 @@ self.addEventListener('install', (e) => {
   })());
 });
 self.addEventListener('fetch', (e) => {
-  console.log(e);
+  e.request = e.request || {};
   e.respondWith((async () => {
-    try{const r = await caches.match(e.request);
+    try{const r = await caches.match(e.request.url);
     if (r) return r;
-    const response = await fetch(e.request);
+    const response = await fetch(e.request.url);
     const cache = await caches.open(cacheName);
-    cache.put(e.request, response.clone());
+    cache.put(e.request.url, response.clone());
     return response;}catch(tgx){}
   })());
 });
