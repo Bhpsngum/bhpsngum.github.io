44544dbe80a611c4abe17bc0c9c3e56eb83d6798
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-05-19 23:35:46 +0700

    test this

--- starblast/mapeditor/beta/sw.js
+++ starblast/mapeditor/beta/sw.js
@@ -3,7 +3,10 @@ const cacheName = "MapEditorCaches";
 self.addEventListener('install', (e) => {
   e.waitUntil((async () => {
     const cache = await caches.open(cacheName);
-    await cache.addAll(imports);
+    imports.forEach(function (imp) {
+      let res = fetch(imp);
+      res.ok && cache.put(imp,res);
+    });
   })());
 });
 self.addEventListener('fetch', (e) => {
