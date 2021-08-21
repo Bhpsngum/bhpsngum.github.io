8b96306d148def29862461e68fd5ccf3e1e0e573
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-08-21 19:40:36 +0900

    update

--- starblast/ship-tree-path-finder/script.js
+++ starblast/ship-tree-path-finder/script.js
@@ -7,40 +7,65 @@
     target.css("color","");
     target.html(html)
   }
+  let uAr = function(array, noSort) {
+    let res = Array.from(new Set(array));
+    if (noSort) return res;
+    return res.sort(function(a,b){return a - b})
+  }
+  let getModel = function(code) {
+    code = getNum(code);
+    let t = internals.models.get(code);
+    if (code != null) return t;
+    return code%100
+  }
+  let getNum = function(num) {
+    let n = Number(num);
+    return isNaN(n)?num:n
+  }
+
+  let submitPath = function(path) {
+    if (path.length > 0) results.push([...path])
+  }
+
+  let getNextShipCodes = function (code, level, path) {
+    code = getNum(code);
+    if (code === parsed_code) return submitPath(path);
+    let custom_next = internals.nexts.get(code);
+    let nextLevel = Number(level) + 1;
+    if (Array.isArray(custom_next)) {
+      let cnext = [], ships = Object.values(internals.ships).flat();
+      for (let type of custom_next) {
+        if (ships.indexOf(type) != -1) cnext.push(type);
+      }
+      return uAr(cnext).forEach((ship, i) => getNextShipCodes(ship, nextLevel, [...path, [nextLevel, ship, i]]));
+    }
+    else {
+      let next_ships = internals.ships[nextLevel];
+      if (!next_ships) return;
+      let current_ships = internals.ships[level];
+      let model = code - level * 100 - 1;
+      let alpha = Math.max(0, Math.round(model / Math.max(current_ships.length - 1, 1) * (next_ships.length - 2)));
+      return next_ships.slice(alpha, alpha + 2).forEach((ship, i) => getNextShipCodes(ship, nextLevel, [...path, [nextLevel, ship, i]]));
+    }
+    return
+  }
+
+  let internals, mod_name, parsed_code, results;
+  let shipInput = $("#ship-input");
+  let shipSelect = $("#ship-select");
+
   let target = $("#results");
-  let findPath = function (){
-    let ship_name = $("#ship-input").val();
-    let mod_name = $("#tree-select").val();
+  let loadTree = function (){
+    mod_name = $("#tree-select").val();
+    let link;
     if (["vanilla", "kest"].indexOf(mod_name) != -1) link = "https://raw.githubusercontent.com/Bhpsngum/starblast/master";
     else link = "https://starblast.data.neuronality.com";
     link += "/mods/" + mod_name + ".js";
-    let game = {custom: {}}
-    for (let i of ["addAlien","addAsteroid","addCollectible","setObject","setCustomMap","setUIComponent","removeObject"]) game[i] = function(){}
-    if (!ship_name || !mod_name) {
-      if (!ship_name) showError("Please enter a ship to lookup");
-      else showError("Please choose a ship tree to lookup")
-    }
-    else {
-      showResults("Processing...");
+    if (mod_name) {
       $.get(link).then(function(mod_code){
+        let game = {custom: {}}
+        for (let i of ["addAlien","addAsteroid","addCollectible","setObject","setCustomMap","setUIComponent","removeObject"]) game[i] = function(){}
         Function("game", mod_code).call(game, game);
-        let internals = game.custom.ships;
-        let uAr = function(array, noSort) {
-          let res = Array.from(new Set(array));
-          if (noSort) return res;
-          return res.sort(function(a,b){return a - b})
-        }
-        let getModel = function(code) {
-          code = getNum(code);
-          let t = internals.models.get(code);
-          if (code != null) return t;
-          return code%100
-        }
-        let getNum = function(num) {
-          let n = Number(num);
-          return isNaN(n)?num:n
-        }
-
         let default_ships = (game.options.reset_tree)?[]:[
           ,
           [101],
@@ -85,50 +110,48 @@
         for (let i in internals.ships) {
           internals.ships[i] = uAr(internals.ships[i]).sort(function (a,b) {return getModel(a) - getModel(b)})
         }
+        shipSelect.html([...internals.names.values()].map(name => `<a href="javascript:void 0" onclick="$('#ship-input').val('${name}');$(window).click()">${name}</a>`).join(""))
+      }).catch(function(e){showError("Failed to get ship tree info")})
+    }
+  }
 
-        let results = [];
+  let filter = function() {
+    var input, filter, ul, li, a, i;
+    input = shipInput;
+    filter = input.val().toUpperCase();
+    a = $("#ship-select>a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
 
-        let parsed_code = [...internals.names.entries()].find(entry => entry[1].toLowerCase() === ship_name.toLowerCase());
-        if (parsed_code) parsed_code = parsed_code[0];
-        else parsed_code = Number(ship_name);
+  let findPath = function () {
+    let ship_name = shipInput.val();
+    if (!ship_name || !mod_name) {
+      if (!mod_name) showError("Please choose a ship tree to lookup");
+      else showError("Please enter a ship to lookup")
+    }
+    else {
+      results = [];
 
-        let submitPath = function(path) {
-          if (path.length > 0) results.push([...path])
-        }
+      parsed_code = [...internals.names.entries()].find(entry => entry[1].toLowerCase() === ship_name.toLowerCase());
+      if (parsed_code) parsed_code = parsed_code[0];
+      else parsed_code = Number(ship_name);
 
-        let getNextShipCodes = function (code, level, path) {
-          code = getNum(code);
-          if (code === parsed_code) return submitPath(path);
-          let custom_next = internals.nexts.get(code);
-          let nextLevel = Number(level) + 1;
-          if (Array.isArray(custom_next)) {
-            let cnext = [], ships = Object.values(internals.ships).flat();
-            for (let type of custom_next) {
-              if (ships.indexOf(type) != -1) cnext.push(type);
-            }
-            return uAr(cnext).forEach((ship, i) => getNextShipCodes(ship, nextLevel, [...path, [nextLevel, ship, i]]));
-          }
-          else {
-            let next_ships = internals.ships[nextLevel];
-            if (!next_ships) return;
-            let current_ships = internals.ships[level];
-            let model = code - level * 100 - 1;
-            let alpha = Math.max(0, Math.round(model / Math.max(current_ships.length - 1, 1) * (next_ships.length - 2)));
-            return next_ships.slice(alpha, alpha + 2).forEach((ship, i) => getNextShipCodes(ship, nextLevel, [...path, [nextLevel, ship, i]]));
-          }
-          return
-        }
+      let starting_ships = internals.ships[1];
 
-        let starting_ships = internals.ships[1];
+      for (let i of starting_ships) getNextShipCodes(i, 1, [[1,i,null]]);
 
-        for (let i of starting_ships) getNextShipCodes(i, 1, [[1,i,null]]);
+      let res_text = `<b>${results.length>0?results.length:"No"} result${results.length!=1?"s":""} found</b>`;
 
-        let res_text = `<b>${results.length>0?results.length:"No"} result${results.length!=1?"s":""} found</b>`;
+      res_text += results.map((path, i) => `<br></br><h3>Path ${i+1}</h3><table><tr><th>Tier</th>${path.map(v => "<th style='width:" + 85/path.length + "%'>" + v[0] + "</th>").join("")}</tr><tr><th>Ship name</th>${path.map(v => "<td>"+internals.names.get(v[1]).replace(/\w+/g,v=>v[0].toUpperCase()+v.slice(1))+"</td>").join("")}</tr><tr><th>Ship code</th>${path.map(v => "<td>"+v[1]+"</td>").join("")}</tr><tr><th>Upgrade option</th>${path.map(v => "<td>"+(v[2] == null ? ("Starting ship" + (starting_ships.length > 1 ? (" " + (starting_ships.indexOf(v[1]) + 1)) : "")) : (v[2] != 0?"0 (Right)":"9 (Left)"))+"</td>").join("")}</tr></table>`).join("");
 
-        res_text += results.map((path, i) => `<br></br><h3>Path ${i+1}</h3><table><tr><th>Tier</th>${path.map(v => "<th style='width:" + 85/path.length + "%'>" + v[0] + "</th>").join("")}</tr><tr><th>Ship name</th>${path.map(v => "<td>"+internals.names.get(v[1]).replace(/\w+/g,v=>v[0].toUpperCase()+v.slice(1))+"</td>").join("")}</tr><tr><th>Ship code</th>${path.map(v => "<td>"+v[1]+"</td>").join("")}</tr><tr><th>Upgrade option</th>${path.map(v => "<td>"+(v[2] == null ? ("Starting ship" + (starting_ships.length > 1 ? (" " + (starting_ships.indexOf(v[1]) + 1)) : "")) : (v[2] != 0?"0 (Right)":"9 (Left)"))+"</td>").join("")}</tr></table>`).join("");
-
-        showResults(res_text);
-      }).catch(function(e){showError("Connection failed.")})
+      showResults(res_text);
     }
   }
 
@@ -138,5 +161,12 @@
   }).reduce((a,b) => (a[b[0]] = b[1],a), {})
 
   if (queries.hidetitle == "true") $("#title").remove();
+  $("#tree-select").on("change", loadTree);
+  shipInput.on("click", function(){shipSelect.css("display","")});
+  $(window).on("click", function(){shipSelect.css("display","none")});
+  $("#ship-choose, #ship-choose>*").on("click", function(e) {
+    e.stopPropagation();
+  });
+  shipInput.on("keyup",filter);
   $("#lookup").on("click",findPath);
 })();
