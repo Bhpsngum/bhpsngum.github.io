81299fbcdbbee1032a1cdc88569873bffae45bfe
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-10-06 23:33:17 +0900

    temp_save

---
+++ starblast/ecp/customization.js
@@ -0,0 +1,299 @@
+window.initCustomSetup = function(initalizer){
+  let Badge = function() {
+    function t(t, e) {
+      this.size = null != t ? t : 128, this.custom = null != e ? e : {}, this.icon = this.custom.badge || "pirate", this.laser = this.custom.laser || "simple", this.finish = this.custom.finish || "gold", this.hue = this.custom.hue || 0, this.IOl1I()
+    }
+    return t.prototype.toImage = function() {
+      return this.image = new Image, this.image.src = this.canvas.toDataURL(), this.image
+    }, t.prototype.updateImage = function() {
+      if (null != this.image) return this.image.src = this.canvas.toDataURL()
+    }, t.prototype.IOl1I = function() {
+      var t, e, i, s, n;
+      if (null == this.canvas && (this.canvas = document.createElement("canvas")), n = this.canvas.width = 2 * this.size, s = this.canvas.height = this.size, e = this.canvas.getContext("2d"), e.clearRect(0, 0, this.canvas.width, this.canvas.height), "blank" !== this.custom.badge) return e.fillStyle = "#000", e.beginPath(), e.arc(n / 2, s / 2, s / 2, 0, 2 * Math.PI, !0), e.fill(), e.beginPath(), e.moveTo(.05 * n, .25 * s), e.lineTo(.05 * n, .75 * s), e.lineTo(n / 2, .9 * s), e.lineTo(.95 * n, .75 * s), e.lineTo(.95 * n, .25 * s), e.lineTo(n / 2, .1 * s), e.closePath(), e.fill(), e.lineWidth = .07 * s, e.globalCompositeOperation = "destination-out", e.strokeStyle = "#000", e.beginPath(), e.arc(n / 2, s / 2, .6 * s, 0, 2 * Math.PI, !0), e.stroke(), this.drawMaterial(e, n, s), this.drawIcon(e, n, s), this.drawLaser(e, n, s), e.globalCompositeOperation = "source-atop", e.save(), e.translate(n / 2, s / 2), e.scale(n / 2, s / 2), i = e.createRadialGradient(0, 0, 0, 0, 0, 1), i.addColorStop(0, "rgba(255,255,255,.2)"), i.addColorStop(1, "rgba(0,0,0,.2)"), e.fillStyle = i, e.fillRect(-1, -1, 2, 2), e.restore(), e.globalCompositeOperation = "source-over", i = e.createRadialGradient(n / 2 - .25 * s, s / 2 - .25 * s, 0, n / 2, s / 2, .45 * s), i.addColorStop(0, "rgba(0,0,0,0)"), i.addColorStop(.5, "rgba(0,0,0,0)"), i.addColorStop(1, "rgba(0,0,0,.5)"), e.fillStyle = i, e.beginPath(), e.arc(n / 2, s / 2, .45 * s, 0, 2 * Math.PI, !0), e.fill(), e.globalCompositeOperation = "destination-over", e.translate(n / 2, s / 2), e.scale(n / 2, s / 2), i = e.createRadialGradient(0, 0, 0, 0, 0, 1), i.addColorStop(.7, "rgba(0,0,0,1)"), i.addColorStop(1, "rgba(0,0,0,0)"), e.fillStyle = i, e.fillRect(-1, -1, 2, 2), this.resize ? (t = document.createElement("canvas"), t.width = this.size, t.height = this.size / 2, t.getContext("2d").drawImage(this.canvas, 0, 0, this.size, this.size / 2), this.canvas = t) : void 0
+    }, t.prototype.drawMaterial = function(t, e, i) {
+      var s, n, l, a, o, r, h, u, d, c;
+      switch (this.finish) {
+        case "alloy":
+          s = t.createLinearGradient(0, 0, 0, i), s.addColorStop(0, "#68A"), s.addColorStop(.5, "#FFF"), s.addColorStop(.5, "#765"), s.addColorStop(1, "#CCC");
+          break;
+        case "titanium":
+          s = t.createLinearGradient(0, 0, 0, i), s.addColorStop(0, "#444"), s.addColorStop(.5, "#AAA"), s.addColorStop(.5, "#444"), s.addColorStop(1, "#111");
+          break;
+        case "gold":
+          s = t.createLinearGradient(0, 0, 0, i), s.addColorStop(0, "hsl(40,100%,50%)"), s.addColorStop(.5, "hsl(40,100%,80%)"), s.addColorStop(.5, "hsl(20,100%,30%)"), s.addColorStop(1, "hsl(40,100%,50%)");
+          break;
+        case "carbon":
+          for (s = t.createLinearGradient(0, 0, 0, i), h = Math.min(10, this.size / 10), l = a = 0, u = h - 1; a <= u; l = a += 1) s.addColorStop(l / h, "#000"), s.addColorStop((l + 1) / h, "#888");
+          for (n = t.createLinearGradient(0, 0, 0, i), n.addColorStop(0, "#333"), n.addColorStop(.1, "#888"), l = o = 0, d = h - 1; o <= d; l = o += 1) n.addColorStop((l + .5) / h, "#000"), n.addColorStop(Math.min(1, (l + 1.5) / h), "#888");
+          break;
+        default:
+          s = t.createLinearGradient(0, 0, 0, i), s.addColorStop(0, "#EEE"), s.addColorStop(1, "#666")
+      }
+      if (t.globalCompositeOperation = "source-atop", t.fillStyle = s, "carbon" === this.finish) {
+        for (l = r = 0, c = 4 * h - 1; r <= c; l = r += 1) t.fillStyle = l % 2 == 0 ? s : n, t.fillRect(l * e / (4 * h), 0, e / (4 * h), i);
+        s = t.createLinearGradient(0, 0, 0, i), s.addColorStop(.3, "rgba(0,0,0,.5)"), s.addColorStop(.5, "rgba(0,0,0,0)"), s.addColorStop(.7, "rgba(0,0,0,.5)"), t.fillStyle = s, t.fillRect(0, 0, e, i)
+      } else t.fillStyle = s, t.fillRect(0, 0, e, i);
+      return t.globalCompositeOperation = "source-over"
+    }, t.prototype.drawLaser = function(t, e, i) {
+      var s, n, l, a, o, r, h, u, d, c, p, I;
+      for (t.save(), t.translate(.12 * e, i / 2), I = l0OlO.getShape(parseInt(this.laser)), n = 0, a = I.length; n < a; n++) {
+        for (p = I[n], t.beginPath(), s = t.createRadialGradient(0, 0, 0, 0, 0, i / 6), s.addColorStop(0, "hsl(50,100%,100%)"), s.addColorStop(1, "hsl(50,80%,40%)"), t.fillStyle = s, t.strokeStyle = "rgba(0,0,0,.8)", t.lineWidth = i / 24, l = 0, o = p.length; l < o; l++) c = p[l], t.lineTo(c[1] * i / 10, c[0] * i / 9);
+        t.closePath(), t.stroke(), t.fill()
+      }
+      for (t.restore(), t.save(), t.translate(.88 * e, i / 2), I = l0OlO.getShape(parseInt(this.laser)), u = 0, r = I.length; u < r; u++) {
+        for (p = I[u], t.beginPath(), s = t.createRadialGradient(0, 0, 0, 0, 0, i / 6), s.addColorStop(0, "hsl(50,100%,100%)"), s.addColorStop(1, "hsl(50,80%,40%)"), t.fillStyle = s, t.strokeStyle = "rgba(0,0,0,.8)", t.lineWidth = i / 24, d = 0, h = p.length; d < h; d++) c = p[d], t.lineTo(c[1] * i / 10, c[0] * i / 9);
+        t.closePath(), t.stroke(), t.fill()
+      }
+      return t.restore()
+    }, t.prototype.drawIcon = function(t, e, i) {
+      var s, n, l, a, o, r, h, u, d, c, p;
+      switch (this.icon) {
+        case "gamepedia":
+          this.icon = "https://starblast.io/ecp/gamepedia.png";
+          break;
+        case "discord":
+          this.icon = "https://starblast.io/ecp/discord.png";
+          break;
+        case "twitch":
+          this.icon = "https://starblast.io/ecp/twitch.png";
+          break;
+        case "medic":
+          this.icon = "https://starblast.io/ecp/medic.jpg";
+          break;
+        case "halo":
+          this.icon = "https://starblast.io/ecp/halo.png";
+          break;
+        case "nwac":
+          this.icon = "https://starblast.io/ecp/nwac.png";
+          break;
+        case "unge":
+          this.icon = "https://starblast.io/ecp/unge.png";
+          break;
+        case "csf":
+          this.icon = "https://starblast.io/ecp/csf.png";
+          break;
+        case "pmf":
+          this.icon = "https://starblast.io/ecp/pmf.png"
+      }
+      if (d = Math.round(i / 2.2), null != this.icon && this.icon.startsWith("http")) {
+        if (this.icon = this.icon.replace("http:", "https:"), null == this.icon_src && (this.icon_src = new Image, this.icon_src.crossOrigin = "Anonymous", this.icon_src.src = this.icon, this.icon_src.onload = function(t) {
+            return function() {
+              return t.IOl1I(), t.updateImage()
+            }
+          }(this)), this.icon_src.complete) try {
+          s = document.createElement("canvas"), s.width = i, s.height = i, n = s.getContext("2d"), n.fillStyle = "#FFF", n.beginPath(), n.arc(i / 2, i / 2, .45 * i, 0, 2 * Math.PI, !0), n.fill(), n.globalCompositeOperation = "source-in", n.drawImage(this.icon_src, .05 * i, .05 * i, .9 * i, .9 * i), t.drawImage(s, e / 2 - .5 * i, i / 2 - .5 * i, i, i)
+        } catch (t) {
+          t
+        }
+      } else {
+        switch (t.font = d + "pt SBGlyphs", t.textBaseline = "middle", t.textAlign = "center", this.icon) {
+          case "reddit":
+            t.fillStyle = "#246";
+            break;
+          case "star":
+            t.fillStyle = "hsl(200,50%,20%)";
+            break;
+          case "youtube":
+            t.fillStyle = "#B11";
+            break;
+          case "paw":
+            t.fillStyle = "#DA5";
+            break;
+          case "pirate":
+          case "invader":
+          case "empire":
+          case "alliance":
+          case "sdf":
+          case "medic":
+            t.fillStyle = "#111";
+            break;
+          default:
+            t.fillStyle = "hsl(200,50%,20%)"
+        }
+        switch (t.beginPath(), t.arc(e / 2, i / 2, .45 * i, 0, 2 * Math.PI, !0), t.fill(), this.icon) {
+          case "reddit":
+            t.fillStyle = "#FFF", t.fillText("~", e / 2, i / 2);
+            break;
+          case "youtube":
+            t.fillStyle = "#FFF", t.fillText("Z", e / 2, i / 2);
+            break;
+          case "pirate":
+            t.font = d + "pt SBGlyphs", t.fillStyle = "#FFF", t.fillText("[", e / 2, i / 2);
+            break;
+          case "sdf":
+            t.font = d + "pt SBGlyphs", t.fillStyle = "#FFF", t.fillText("Y", e / 2, i / 2);
+            break;
+          case "paw":
+            t.fillStyle = "#000", t.fillText("V", e / 2, i / 2);
+            break;
+          case "alliance":
+            t.fillStyle = "#F00", t.fillText("X", e / 2, i / 2);
+            break;
+          case "empire":
+            t.fillStyle = "#FFF", t.fillText("R", e / 2, i / 2);
+            break;
+          case "invader":
+            for (u = [
+                [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+                [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+                [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+                [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0]
+              ], t.fillStyle = "hsl(120,100%,50%)", l = .7 * i / 11, a = r = 0; r <= 10; a = r += 1)
+              for (o = h = 0; h <= 7; o = h += 1) c = e / 2 + l * (a - 5), p = i / 2 + l * (o - 4), 1 === u[o][a] && t.fillRect(c - .4 * l, p - .4 * l, .8 * l, .8 * l);
+            break;
+          default:
+            t.fillStyle = "hsl(50,100%,70%)", t.fillText("S", e / 2, i / 2)
+        }
+      }
+    }, t.prototype.drawStar = function(t, e, i, s, n) {
+      var l, a, o, r;
+      for (t.beginPath(), l = -Math.PI / 2, o = 0; o <= 4; o += 1) a = Math.cos(l), r = Math.sin(l), t.lineTo(e + a * n, i + r * n), l += Math.PI / 5, a = Math.cos(l), r = Math.sin(l), t.lineTo(e + a * s, i + r * s), l += Math.PI / 5;
+      return t.closePath(), t.fill()
+    }, t.prototype.drawWing = function(t, e, i, s, n, l) {
+      var a, o, r, h, u, d, c, p, I;
+      for (null == l && (l = "#FFF"), t.save(), t.translate(e / 2, i / 2), t.strokeStyle = "#000", t.fillStyle = l, t.lineWidth = .03 * i, t.lineCap = "round", t.beginPath(), a = o = 0, d = s.length - 1; o <= d; a = o += 1) t.lineTo(s[a], n[a]);
+      for (t.closePath(), t.fill(), a = r = 0, c = s.length - 1; r <= c; a = r += 1) t.beginPath(), t.moveTo(s[a], n[a]), t.lineTo(s[(a + 1) % s.length], n[(a + 1) % s.length]), t.stroke();
+      for (t.beginPath(), a = h = 0, p = s.length - 1; h <= p; a = h += 1) t.lineTo(-s[a], n[a]);
+      for (t.closePath(), t.fill(), a = u = 0, I = s.length - 1; u <= I; a = u += 1) t.beginPath(), t.moveTo(-s[a], n[a]), t.lineTo(-s[(a + 1) % s.length], n[(a + 1) % s.length]), t.stroke();
+      return t.restore()
+    }, t.table = [], t.lO0O0 = function(e, i, s, n, l) {
+      var a, o;
+      return null == l && (l = 0), o = e + i + s + n + l, t.table[o] || (a = new t(n, {
+        badge: e,
+        laser: i,
+        finish: s,
+        hue: l
+      }).toImage(), t.table[o] = a), t.table[o]
+    }, t.getRandomImage = function(e) {
+      var i, s, n;
+      return i = ["star", "paw", "pirate", "reddit", "invader", "youtube"][Math.floor(6 * Math.random())], s = ["zinc", "gold", "alloy", "carbon", "titanium"][Math.floor(5 * Math.random())], n = ["0", "1", "2", "3"][Math.floor(4 * Math.random())], t.lO0O0(i, n, s, e)
+    }, t.test = function() {
+      var e, i, s, n, l, a, o, r, h, u, d, c;
+      for (u = ["zinc", "gold", "alloy", "carbon", "titanium"], s = 0, a = u.length; s < a; s++)
+        for (e = u[s], d = ["star", "paw", "pirate", "reddit", "invader", "youtube"], n = 0, o = d.length; n < o; n++)
+          for (i = d[n], c = ["simple"], h = 0, r = c.length; h < r; h++) l = c[h], document.body.appendChild(new t(128, {
+            badge: i,
+            laser: l,
+            finish: e
+          }).canvas)
+    }, t
+  }(), l0OlO = function() {
+    function t() {}
+    return t.createTexture = function() {
+      var e, i, s, n, l, a, o, r;
+      for (e = document.createElement("canvas"), e.width = 2048, e.height = 1024, this.shapes = [], this.shapes.push(t.shape1()), this.shapes.push(t.shape2()), this.shapes.push(t.shape3()), this.shapes.push(t.shape4()), this.shapes.push(t.shape5()), this.shapes.push(t.shape6()), this.shapes.push(t.shape7()), i = e.getContext("2d"), i.translate(0, e.height), i.scale(e.width / 8, -e.width / 8), i.translate(.5, .5), i.scale(.5, .5), n = [.5, .1, .04, .1, .5], o = this.shapes, s = l = 0, a = o.length; l < a; s = ++l) r = o[s], i.save(), i.translate(s % 5 * 3, 3 * Math.floor(s / 5)), this.drawGradient(i, n[s], s), this.drawLaser(i, r), i.restore();
+      return e
+    }, t.drawGradient = function(t, e, i) {
+      var s;
+      if (null == e && (e = .5), null == i && (i = 0), s = t.createRadialGradient(0, 0, 0, 0, 0, 1), s.addColorStop(0, "hsla(10,100%,100%," + e + ")"), s.addColorStop(1, "hsla(10,100%,100%,0)"), t.fillStyle = s, t.fillRect(-1, -1, 2, 2), 1 === i) return t.fillStyle = "#000", t.fillRect(-1, -.025, 2, .05)
+    }, t.getShape = function(t) {
+      switch (t) {
+        case 3:
+          return this.shape4();
+        case 1:
+          return this.shape2();
+        case 2:
+          return this.shape3();
+        default:
+          return this.shape1()
+      }
+    }, t.shape1 = function() {
+      var t, e, i, s, n, l;
+      for (s = [], e = i = 0; i <= 20; e = i += 1) t = e / 20 * Math.PI * 2, n = Math.cos(t), l = Math.sin(t), n = n < 0 ? -Math.sqrt(-n) : Math.sqrt(n), l = l < 0 ? -Math.sqrt(-l) : Math.sqrt(l), s.push([n, l / 3]);
+      return [s]
+    }, t.shape2 = function() {
+      var t, e, i, s, n, l, a, o;
+      for ([], n = [], e = i = 0; i <= 20; e = i += 1) t = e / 20 * Math.PI * 2, a = Math.cos(t), o = Math.sin(t), a = a < 0 ? -Math.sqrt(-a) : Math.sqrt(a), o = o < 0 ? -1 : Math.sqrt(o), n.push([1.4 * a, .2 + o / 10]);
+      for (l = [], e = s = 0; s <= 20; e = s += 1) t = e / 20 * Math.PI * 2, a = Math.cos(t), o = Math.sin(t), a = a < 0 ? -Math.sqrt(-a) : Math.sqrt(a), o = o < 0 ? -Math.sqrt(-o) : 1, l.push([1.4 * a, o / 10 - .2]);
+      return [n, l]
+    }, t.shape3 = function() {
+      return [
+        [
+          [2, 0],
+          [1, .1],
+          [.55, .8],
+          [.35, -.1],
+          [.05, .8],
+          [-.25, -.1],
+          [-.55, .8],
+          [-1, .1],
+          [-2, 0],
+          [-1, -.1],
+          [-.85, -.8],
+          [-.55, .1],
+          [-.25, -.8],
+          [.05, .1],
+          [.35, -.8],
+          [.55, .1],
+          [.75, -.8],
+          [1, -.1],
+          [2, 0]
+        ]
+      ]
+    }, t.shape4 = function() {
+      return [
+        [
+          [1.4, -.6],
+          [1.1, -.6],
+          [1.1, .6],
+          [1.4, .6]
+        ],
+        [
+          [.55, -.6],
+          [.25, -.6],
+          [.25, .6],
+          [.55, .6]
+        ],
+        [
+          [-.55, -.6],
+          [-.25, -.6],
+          [-.25, .6],
+          [-.55, .6]
+        ],
+        [
+          [-1.4, -.6],
+          [-1.1, -.6],
+          [-1.1, .6],
+          [-1.4, .6]
+        ]
+      ]
+    }, t.shape5 = function() {
+      var t, e, i, s, n, l, a, o;
+      for (l = [], t = [0, 70, 90, 110, 180, 250, 270, 290, 360], s = [1, 1, .7, 1, 1, 1, .7, 1, 1], e = i = 0, n = s.length - 1; i <= n; e = i += 1) a = Math.cos(t[e] * Math.PI / 180) * s[e], o = Math.sin(t[e] * Math.PI / 180) * s[e], l.push([a, o / 2]);
+      return [l]
+    }, t.shape6 = function() {
+      return [
+        [
+          [2, .4],
+          [2, -.4],
+          [-2, -.4],
+          [-2, .4]
+        ],
+        [
+          [.4, 2],
+          [.4, -2],
+          [-.4, -2],
+          [-.4, 2]
+        ]
+      ]
+    }, t.shape7 = function() {
+      var t, e, i, s, n, l;
+      for (s = [], e = i = 0; i <= 20; e = i += 1) t = e / 20 * Math.PI * 2, n = Math.cos(t), l = Math.sin(t), s.push([n, l]);
+      return [s]
+    }, t.drawLaser = function(t, e) {
+      var i, s, n, l, a, o, r, h;
+      for (i = t.createRadialGradient(0, 0, 0, 0, 0, .3), i.addColorStop(0, "rgba(255,255,255,1)"), i.addColorStop(1, "rgba(255,255,255,.5)"), t.fillStyle = i, r = [], s = 0, l = e.length; s < l; s++) {
+        for (h = e[s], t.beginPath(), n = 0, a = h.length; n < a; n++) o = h[n], t.lineTo(.3 * o[0], .3 * o[1]);
+        t.closePath(), r.push(t.fill())
+      }
+      return r
+    }, t
+  }(),
+  initalizer.createBadge = function (size, image_url, needCover, laser, finish) {
+
+  }
+}
