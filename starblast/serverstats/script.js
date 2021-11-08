93ce63de6ed2178db2406a7d87074535a77a7546
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-11-09 00:34:01 +0900

    save_test

--- starblast/serverstats/script.js
+++ starblast/serverstats/script.js
@@ -1,14 +1,46 @@
 (function(){
-  var updated = false, servers = [], formatDate = function(date) {
-    return new Date(date).toLocaleDateString('en-US',{weekday:'long',year:'numeric',month:'long',day:'numeric'})
+  var updated = false, init, servers = [], IPs = {}, getTime = function(ms) {
+    let days = Math.trunc(ms / 1000 / 60 / 60 / 24), text;
+    if (days < 1) text = "<1 day";
+    else if (days == 1) text = "1 day";
+    else text = days + " days";
+    return text
   }, showText = function (text) {
     text = text || "";
     $("#welcome-text").css("display", text?"":"none").html(text)
+  }, setAdblocker = function (bool) {
+    $("#adblocker").css("display", bool?"":"none")
+  }, fetchLocation = function(ip, action) {
+    $.get("https://ipapi.co/"+ip+"/country_name/").then(function (data) {
+      IPs[ip] = data;
+      localStorage.setItem("server-ips", JSON.stringify(IPs));
+      setAdblocker(false);
+      if ("function" == typeof action) action();
+    }).catch(function(error) {
+      if (error.status == 0) setAdblocker(true);
+      else setAdblocker(false);
+      console.log(error);
+    })
+  }, assignLocation = function(ID, ip) {
+    $("#serverstats #location-"+ID).html("<b>Location:</b> "+ (IPs[ip] || "Unknown"));
+  }, getLocation = function (server) {
+    let serverID = getID(server), ip = String(server.address).split(":")[0], setLocation = function() { assignLocation(serverID, ip) }
+    setLocation();
+    fetchLocation(ip, setLocation)
   }, getID = function(server) {
     return String(server.address).replace(/\./g, "-").replace(/\:/g, "_")
   }, serverStatBox = function(server, index) {
-    let html = `<img src='servericon.jpg' onerror="setTimeout(function(){this.src = this.src}.bind(this),5000)">
-    <h3 style="text-align:center">Server ${index}</h3>`;
+    let serverID = getID(server), html = `<img src='servericon.jpg' onerror="setTimeout(function(){this.src = this.src}.bind(this),5000)">
+    <h3 style="text-align:center">Server ${index + 1} ${server.modding?'<img src="favicon.ico" class="modding-thumnail" title="This is a Modding server" onerror="setTimeout(function(){this.src = this.src}.bind(this),5000)">':""}</h3>
+    <p><b>Region:</b> ${server.location}</p>
+    <p id="location-${serverID}"></p>
+    <p><b>Uptime:</b> ${getTime((server.usage||{}).elapsed || 0)}</p>
+    <p><b>Players:</b> ${server.current_players}</p>
+    <p><b>Systems:</b> ${server.systems.length}</p>`, el = $("#serverstats>.serverStatBox#"+serverID);
+    if (!el[0]) el = $(`<div id=${serverID} class="serverStatBox">${html}</div>`);
+    else el.html(html);
+    $("#serverstats").append(el);
+    getLocation(server)
   }, getNum = function(num) {
     num = num.toString();
     let str = [];
@@ -19,12 +51,11 @@
     let t = Math.trunc(ms/3600), u = Math.trunc((ms-t*3600)/60);
     return [t,u,Math.trunc(ms-t*3600-u*60)].map(i => Math.max(i,0)).map(i => i<10?"0"+i.toString():i).join(":")
   }, loadInfos = function() {
-    servers.forEach(serverStatBox);
     let serverStats = $("#serverstats");
     serverStats.append($("#serverstats>#welcome-text")[0] || '<p style="text-align:center;font-size:15pt" id="welcome-text">Loading data...</p>');
-    servers.forEach(server => serverStats.append($("#serverstats>.serverStatBox#"+getID(server))));
+    servers.forEach(serverStatBox);
     let elist = $("#serverstats>*");
-    while (elist.length > servers.length + 1) $(Array.prototype.shift.call(list)).remove();
+    while (elist.length > servers.length + 1) $(Array.prototype.shift.call(elist)).remove();
     if (servers.length == 0) showText("No servers are active right now.");
     else showText()
   }, setStatus = function(n) {
@@ -41,11 +72,10 @@
     $.getJSON("https://starblast.io/simstatus.json").then(function(simstatus) {
       servers = Array.from(simstatus || 0);
       setStatus(0);
+      loadInfos();
       if (!init) {
-        loadInfos();
         adjustwidth();
         setTimeout(adjustwidth, 1);
-        setInterval(loadInfos, 1000);
         init = !0;
       }
       queueNextUpdate();
@@ -54,7 +84,7 @@
       queueNextUpdate();
     });
   }, queueNextUpdate = function() {
-    setTimeout(function(){updated = false}, 5000)
+    setTimeout(function(){updated = false}, 10000)
   }, img_size = 360, padding_ratio = 1/30, full_ratio = 1+4*padding_ratio, adjustwidth = function(){
     let g = $(window).width(), x = Math.round(g/(img_size*full_ratio)), t = g/(x||1)/full_ratio, m = Math.trunc(t*padding_ratio);
     $(":root").css({
@@ -69,6 +99,7 @@
       update()
     }
   }
+  try {IPs = JSON.parse(localStorage.getItem("server-ips")) || {}} catch (e) {}
   window.addEventListener("resize", adjustwidth);
   setInterval(checkUpdate,1)
 })();
