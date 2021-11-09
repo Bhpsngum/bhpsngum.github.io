159e7a65e201fa43b1eb2ea399486dde86529935
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-11-09 13:48:31 +0900

    test

--- starblast/serverstats/script.js
+++ starblast/serverstats/script.js
@@ -35,7 +35,7 @@
     return String(server.address).replace(/\./g, "-").replace(/\:/g, "_")
   }, serverStatBox = function(server) {
     server = server || {}
-    let serverID = getID(server), html = `<img src='servericon.jpg' onerror="setTimeout(function(){this.src = this.src}.bind(this),5000)">
+    let serverID = getID(server), html = `<img src='servericon.jpg' class='flag' onerror="setTimeout(function(){this.src = this.src}.bind(this),5000)">
     <h3 style="text-align:center">Server ${(server.usage||{}).pid||0}-${(server.usage||{}).ppid||0} ${server.modding?'<img src="favicon.ico" class="modding-thumnail" title="This is a Modding server" onerror="setTimeout(function(){this.src = this.src}.bind(this),5000)">':""}</h3>
     <p><b>Region:</b> ${server.location}</p>
     <p class="location" id="location-${serverID}"></p>
