47736e38ff849f4bd1dc24922320820c2921b8a1
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-11-11 18:59:39 +0900

    add some clans

--- starblast/discord-clans/script.js
+++ starblast/discord-clans/script.js
@@ -5,7 +5,7 @@
   }, basicrev = function (j) {
     return j.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"&apos;").replace(/"/g,"&quot;");
   }
-  let headers = joinData(["Icon", "Clan Name", "Clan Tag", "Active Region(s)", "Description", "Public Invite?", "Invite Link", "How to join"], true);
+  let headers = joinData(["Icon", "Clan/Group Name", "Clan/Group Tag (if exists)", "Active Region(s)", "Description", "Public Invite?", "Invite Link", "How to join"], true);
   $.getJSON("clans.json").then(function (data){
     $("#clans-list").html(headers+data.sort((a,b) => a.name < b.name ? -1 : 1).map(clan => joinData([`<img src="${basicrev(clan.icon)}">`, basicrev(clan.name), `[${basicrev(clan.tag)}]`, clan.active_regions.join(", "), basicrev(clan.description || "No description"), clan.invite_open ? "yes" : "no", clan.invite_open ? `<a target="_blank" href = "${basicrev(clan.invite_link)}">${bais
       (clan.invite_link)}</a>` : "", clan.invite_open ? "" : (basicrev(clan.how_to_apply) || "Unknown")])))
