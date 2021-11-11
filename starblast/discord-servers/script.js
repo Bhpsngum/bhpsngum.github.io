8a0a753cc1c401a668dc01b1f1632216cfe3fe44
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-11-11 19:26:00 +0900

    project name changed

--- starblast/discord-servers/script.js
+++ starblast/discord-servers/script.js
@@ -5,8 +5,8 @@
   }, basicrev = function (j) {
     return j.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"&apos;").replace(/"/g,"&quot;");
   }
-  let headers = joinData(["Icon", "Clan/Group Name", "Clan/Group Tag (if exists)", "Active Region(s)", "Description", "Public Invite?", "Invite Link", "How to join"], true);
-  $.getJSON("clans.json").then(function (data){
+  let headers = joinData(["Icon", "Server Name", "Server Type", "Active Region(s)", "Description", "Public Invite?", "Invite Link", "How to join"], true);
+  $.getJSON("servers.json").then(function (data){
     $("#clans-list").html(headers+data.sort((a,b) => a.name < b.name ? -1 : 1).map(clan => joinData([`<img src="${basicrev(clan.icon)}">`, basicrev(clan.name), `[${basicrev(clan.tag)}]`, clan.active_regions.join(", "), basicrev(clan.description || "No description"), clan.invite_open ? "yes" : "no", clan.invite_open ? `<a target="_blank" href = "${basicrev(clan.invite_link)}">${basicrev
       (clan.invite_link)}</a>` : "", clan.invite_open ? "" : (basicrev(clan.how_to_apply) || "Unknown")])))
   }).fail(e => alert("Failed! Please reload the page again!"))
