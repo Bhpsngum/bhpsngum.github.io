a056fbbc5875a832d8af09c42c1c63ef978edc5d
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-11-11 19:56:26 +0900

    test

--- starblast/discord-servers/script.js
+++ starblast/discord-servers/script.js
@@ -3,11 +3,14 @@
     let tag = isHeader ? "th" : "td"
     return "<tr>"+array.map(v => `<${tag}>${v}</${tag}>`).join("")+"</tr>"
   }, basicrev = function (j) {
-    return j.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"&apos;").replace(/"/g,"&quot;");
-  }
+    return String(j).replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"&apos;").replace(/"/g,"&quot;");
+  }, servertypes = ["server", "event", "mod", "others"], regions = ["Asia-Australia", "America", "Brazil", "Europe"];
   let headers = joinData(["Icon", "Server Name", "Server Type", "Active Region(s)", "Description", "Public Invite?", "Invite Link", "How to join"], true);
   $.getJSON("servers.json").then(function (data){
-    $("#clans-list").html(headers+data.sort((a,b) => a.name < b.name ? -1 : 1).map(clan => joinData([`<img src="${basicrev(clan.icon)}">`, basicrev(clan.name), `[${basicrev(clan.tag)}]`, clan.active_regions.join(", "), basicrev(clan.description || "No description"), clan.invite_open ? "yes" : "no", clan.invite_open ? `<a target="_blank" href = "${basicrev(clan.invite_link)}">${basicrev
-      (clan.invite_link)}</a>` : "", clan.invite_open ? "" : (basicrev(clan.how_to_apply) || "Unknown")])))
+    $("#servers-list").html(headers+data.sort((a,b) => a.name < b.name ? -1 : 1).map(server => {
+      let servername = basicrev(server.name + (server.tag ? ` [${server.tag}]` : "")), servertype = basicrev(server.type);
+      return joinData([`<img src="${basicrev(server.icon)}">`, servername, servertype.slice(0,1).toUpperCase()+servertype.slice(1), server.active_regions.join(", "), basicrev(server.description || "No description"), server.invite_open ? "yes" : "no", server.invite_open ? `<a target="_blank" href = "${basicrev(server.invite_link)}">${basicrev
+      (server.invite_link)}</a>` : "", server.invite_open ? "" : (basicrev(server.how_to_apply) || "Unknown")]))
+    })
   }).fail(e => alert("Failed! Please reload the page again!"))
 })();
