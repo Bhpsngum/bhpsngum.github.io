501143086a49abb1286a5a2f4d9f9fe7af66fd0f
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2022-04-11 22:11:35 +0900

    css

--- starblast/starblast-modding/scripts/default.js
+++ starblast/starblast-modding/scripts/default.js
@@ -4,5 +4,26 @@
     document.querySelector(".container-overview").remove();
     elem.remove()
   }
-  document.querySelector(".signature").remove()
+  for (let i of document.querySelectorAll(".type-signature")) {
+      if (i.innerText.match(/^\s*:[^]+/) || i.innerText.match(/^\s*→\s*{[^]+}/)) i.remove();
+      else {
+        let text = (i.innerText.match(/\w+/) || [""])[0];
+        i.setAttribute("class", i.getAttribute("class") + " " + text);
+        i.innerText = text.toUpperCase()
+      }
+  }
+  document.querySelector(".signature").remove();
+
+  let article = document.querySelector("article"), children = article.children, createSpacing = function () {
+    let t = document.createElement("div");
+    t.setAttribute("class", "spacing");
+    return t
+  }
+
+  for (let i = 0; i < children.length - 1; ++i) {
+    if (children[i].tagName.toLowerCase() == "dl") {
+      children[i].style.marginBottom = "20px";
+      if (children[i + 1].tagName.toLowerCase() == "h4" && Array.prototype.includes.call(children[i + 1].classList, "name")) article.insertBefore(createSpacing(), children[++i])
+    }
+  }
 })()
