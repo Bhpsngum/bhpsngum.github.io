a60c22341acf27017ef1fa16722fd67ad6bab2f4
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2022-04-14 14:54:13 +0900

    hmm

--- starblast/starblast-modding/scripts/general.js
+++ starblast/starblast-modding/scripts/general.js
@@ -1,7 +1,7 @@
 (function(){
   let hash = window.location.hash.replace(/^#\/*/, "").replace("#", ".html#"), iframe = document.querySelector("#docpage");
   $.get("./" + hash).then(function(data, status, xhr) {
-    console.log(xhr)
+    iframe.src = "./" + (xhr.getResponseHeader("Content-Type").includes("text/html") ? hash : "404.html")
   })
   .catch(function(e){ iframe.src = "./404.html" });
   window.addEventListener("message", function (event) {
