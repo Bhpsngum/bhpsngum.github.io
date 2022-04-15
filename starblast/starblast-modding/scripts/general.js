30382156a573b469d4f969386aa84145f0332529
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2022-04-16 01:49:39 +0900

    check

--- starblast/starblast-modding/scripts/general.js
+++ starblast/starblast-modding/scripts/general.js
@@ -1,12 +1,5 @@
 (function(){
-  let vSelect = $("#versions");
-  $.getJSON("./versions.json").then(function (data) {
-    vSelect.append(data.map((i, j) => `<option value="${i}">${i + (j == 0 ? " (latest)" : "")}</option>`).join(""));
-    vSelect.on("change", function () {
-      let selectedVal = vSelect.val();
-      if (data.includes(selectedVal)) iframe.src = "./" + selectedVal
-    });
-
+  let vSelect = $("#versions"), loadPage = function () {
     let hash = window.location.hash.replace(/^#\/*/, "").replace("#", ".html#"), iframe = document.querySelector("#docpage"), matches = (hash.match(/[^\/]+/) || [])[0] || "";;
     $.get("./" + hash).then(function(d, status, xhr) {
       if (xhr.getResponseHeader("Content-Type").includes("text/html")) {
@@ -27,7 +20,16 @@
     .catch(function(e){
       iframe.src = "./404.html";
       vSelect.val(data.includes(matches) ? matches : data[0])
+    })
+  }
+  $.getJSON("./versions.json").then(function (data) {
+    vSelect.append(data.map((i, j) => `<option value="${i}">${i + (j == 0 ? " (latest)" : "")}</option>`).join(""));
+    vSelect.on("change", function () {
+      let selectedVal = vSelect.val();
+      if (data.includes(selectedVal)) iframe.src = "./" + selectedVal
     });
+    loadPage();
+    window.addEventListener("hashchange", loadPage);
   }).catch(e => window.location.reload());
   window.addEventListener("message", function (event) {
     if (event.origin != "https://bhpsngum.github.io") return;
