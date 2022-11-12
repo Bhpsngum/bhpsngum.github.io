184a80521d698c04fb6c4fa436c7fe0216930f2c
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2022-11-13 01:27:20 +0900

    no recursive load

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 (function () {
   let clearData = function () {
-    if (confirm("Do you really want to remove all local data associating to this domain? This action is irreversible.")) localData.clear(true);
+    if (confirm("Do you really want to remove all local data associating to this domain? This action is irreversible.")) {
+      localStorage.clear();
+      window.open("./", "_self");
+    }
   }
   if (window.location.search.toLowerCase() == "?clearalldata") clearData();
   let x = document.querySelector("#cleardata");
