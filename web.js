8f7c2f3be5f0fce670c6f9b5bfe751789dd03fad
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2022-11-13 01:07:52 +0900

    fix

--- web.js
+++ web.js
@@ -2,7 +2,7 @@
   let clearData = function () {
     if (confirm("Do you really want to remove all local data associating to this domain? This action is irreversible.")) localData.clear(true);
   }
-  if (window.location.search.toLowercase() == "?clearalldata") clearData();
+  if (window.location.search.toLowerCase() == "?clearalldata") clearData();
   let x = document.querySelector("#cleardata");
   if (x != null) x.addEventListener("click", clearData);
 })();
