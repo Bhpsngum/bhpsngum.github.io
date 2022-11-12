7a6e48d801c737f3cc9ebec566a73bb717de5cd2
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2022-11-12 23:58:13 +0900

    push

--- web.js
+++ web.js
@@ -1,1 +1,8 @@
-
+(function () {
+  let clearData = function () {
+    if (confirm("Do you really want to remove all local data associating to this page? This action is irreversible.")) localData.clear(true);
+  }
+  if (window.location.search.toLowercase() == "?clearalldata") clearData();
+  let x = document.querySelector("#cleardata");
+  if (x != null) x.addEventListener("click", clearData);
+});
