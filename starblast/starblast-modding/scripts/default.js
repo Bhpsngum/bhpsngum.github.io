1f11f8dfc7e36a4237d6131ecd65e36deca2e7dd
Bhpsngum <bhpsngumproductions@gmail.com>
2024-05-07 22:03:07 +0900

    more desc

--- starblast/starblast-modding/scripts/default.js
+++ starblast/starblast-modding/scripts/default.js
@@ -55,7 +55,7 @@
     if (href.includes(".js")) e.href = "https://github.com/bhpsngum/starblast-modding/blob/" + window.location.pathname.match(/\/(v[^\/]+)(\/|$)/)[1] + "/src/" + href;
   });
 
-  document.querySelectorAll(".description, .param-desc").forEach(function (e) {
+  document.querySelectorAll(".description, .param-desc, .class-description").forEach(function (e) {
     e.innerHTML = e.innerHTML.replace(/\`([^\`]+?)\`/g, "<code>$1</code>")
   });
 
