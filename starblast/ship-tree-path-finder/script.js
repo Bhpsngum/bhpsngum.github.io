48f00afa89921a98abad8fcaf6991883c4c53506
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2025-11-16 02:55:06 +0900

    add search from url for tree finder

--- starblast/ship-tree-path-finder/script.js
+++ starblast/ship-tree-path-finder/script.js
@@ -55,115 +55,118 @@
     let shipSelect = $("#ship-select");
 
     let target = $("#results");
-    let loadTree = function() {
-        mod_name = $("#tree-select").val();
-        let link;
-        switch (mod_name) {
-            case "mcst_t7_ver":
-            case "mcst_beta":
-                link = "https://raw.githubusercontent.com/Bhpsngum/starblast-snippets/master";
-                break;
-            case "strawberry":
-            case "vanilla":
-                link = "https://raw.githubusercontent.com/pmgl/starblast-modding/master";
-                break;
-            default:
-                link = "https://starblast.data.neuronality.com";
-        }
-        link += "/mods/" + mod_name + ".js";
-        if (mod_name) {
-            $.get(link).then(function(mod_code) {
-                let game = { custom: {} }
-                for (let i of["addAlien", "addAsteroid", "addCollectible", "setObject", "setCustomMap", "setUIComponent", "removeObject"]) game[i] = function() {}
-                Function("game", mod_code).call(game, game);
+    let loadTree = function(value) {
+        return new Promise((res, rej) => {
+            mod_name = value || $("#tree-select").val();
+            let link;
+            switch (mod_name) {
+                case "mcst_t7_ver":
+                case "mcst_beta":
+                    link = "https://raw.githubusercontent.com/Bhpsngum/starblast-snippets/master";
+                    break;
+                case "strawberry":
+                case "vanilla":
+                    link = "https://raw.githubusercontent.com/pmgl/starblast-modding/master";
+                    break;
+                default:
+                    link = "https://starblast.data.neuronality.com";
+            }
+            link += "/mods/" + mod_name + ".js";
+            if (mod_name) {
+                $.get(link).then(function(mod_code) {
+                    let game = { custom: {} }
+                    for (let i of ["addAlien", "addAsteroid", "addCollectible", "setObject", "setCustomMap", "setUIComponent", "removeObject"]) game[i] = function() {}
+                    Function("game", mod_code).call(game, game);
 
-                let default_specs = [
-                    [101, "Fly"],
-                    [201, "Delta-Fighter"],
-                    [202, "Trident"],
-                    [301, "Pulse-Fighter"],
-                    [302, "Side-Fighter", [403, 404]],
-                    [303, "Shadow X-1"],
-                    [304, "Y-Defender"],
-                    [401, "Vanguard"],
-                    [402, "Mercury"],
-                    [403, "X-Warrior"],
-                    [404, "Side-Interceptor"],
-                    [405, "Pioneer"],
-                    [406, "Crusader"],
-                    [501, "U-Sniper"],
-                    [502, "FuryStar"],
-                    [503, "T-Warrior"],
-                    [504, "Aetos"],
-                    [505, "Shadow X-2"],
-                    [506, "Howler"],
-                    [507, "Bat-Defender"],
-                    [601, "Advanced-Fighter"],
-                    [602, "Scorpion"],
-                    [603, "Marauder"],
-                    [604, "Condor"],
-                    [605, "A-Speedster"],
-                    [606, "Rock-Tower"],
-                    [607, "Barracuda"],
-                    [608, "O-Defender"],
-                    [701, "Odyssey"],
-                    [702, "Shadow X-3"],
-                    [703, "Bastion"],
-                    [704, "Aries"]
-                ];
+                    let default_specs = [
+                        [101, "Fly"],
+                        [201, "Delta-Fighter"],
+                        [202, "Trident"],
+                        [301, "Pulse-Fighter"],
+                        [302, "Side-Fighter", [403, 404]],
+                        [303, "Shadow X-1"],
+                        [304, "Y-Defender"],
+                        [401, "Vanguard"],
+                        [402, "Mercury"],
+                        [403, "X-Warrior"],
+                        [404, "Side-Interceptor"],
+                        [405, "Pioneer"],
+                        [406, "Crusader"],
+                        [501, "U-Sniper"],
+                        [502, "FuryStar"],
+                        [503, "T-Warrior"],
+                        [504, "Aetos"],
+                        [505, "Shadow X-2"],
+                        [506, "Howler"],
+                        [507, "Bat-Defender"],
+                        [601, "Advanced-Fighter"],
+                        [602, "Scorpion"],
+                        [603, "Marauder"],
+                        [604, "Condor"],
+                        [605, "A-Speedster"],
+                        [606, "Rock-Tower"],
+                        [607, "Barracuda"],
+                        [608, "O-Defender"],
+                        [701, "Odyssey"],
+                        [702, "Shadow X-3"],
+                        [703, "Bastion"],
+                        [704, "Aries"]
+                    ];
 
-                let default_options = {
-                    ships: game.options.reset_tree ? [] : Array(8).fill(0).map((i, j) => default_specs.filter(s => Math.trunc(s[0] / 100) === j).map(i => i[0])),
-                    nexts: new Map(game.options.reset_tree ? [] : default_specs.filter(s => s[2]).map(i => [i[0], i[2]])),
-                    names: new Map(game.options.reset_tree ? [] : default_specs.map(i => [i[0], i[1]])),
-                    models: new Map(),
-                    levels: new Map()
-                }
-                game.custom.ships = default_options;
-                internals = game.custom.ships;
-                if (Array.isArray(game.options.ships))
-                    for (let ship of game.options.ships) {
-                        try {
-                            let prs = JSON.parse(ship);
-                            let code = getNum(prs.typespec.code);
-                            let next = getNum(prs.typespec.next);
-                            let level = getNum(prs.typespec.level);
-                            if (!Array.isArray(internals.ships[level])) internals.ships[level] = [];
-                            internals.ships[level].push(code);
-                            internals.names.set(code, prs.name.replace(/_/g, " "));
-                            internals.levels.set(code, { level: prs.level, model: prs.model });
-                            if (prs.typespec.model !== code % 100) internals.models.set(code, prs.typespec.model);
-                            let cnxt = uAr(Array.isArray(next) ? next : []);
-                            if (cnxt.length > 0) internals.nexts.set(code, cnxt)
-                        } catch (e) {
-                            game.custom.ships = default_options;
-                            internals = game.custom.ships;
-                            break
-                        }
+                    let default_options = {
+                        ships: game.options.reset_tree ? [] : Array(8).fill(0).map((i, j) => default_specs.filter(s => Math.trunc(s[0] / 100) === j).map(i => i[0])),
+                        nexts: new Map(game.options.reset_tree ? [] : default_specs.filter(s => s[2]).map(i => [i[0], i[2]])),
+                        names: new Map(game.options.reset_tree ? [] : default_specs.map(i => [i[0], i[1]])),
+                        models: new Map(),
+                        levels: new Map()
                     }
-                for (let i in internals.ships) {
-                    internals.ships[i] = uAr(internals.ships[i]).sort(function(a, b) {
-                        let al = internals.levels.get(a) || {},
-                            bl = internals.levels.get(b) || {};
+                    game.custom.ships = default_options;
+                    internals = game.custom.ships;
+                    if (Array.isArray(game.options.ships))
+                        for (let ship of game.options.ships) {
+                            try {
+                                let prs = JSON.parse(ship);
+                                let code = getNum(prs.typespec.code);
+                                let next = getNum(prs.typespec.next);
+                                let level = getNum(prs.typespec.level);
+                                if (!Array.isArray(internals.ships[level])) internals.ships[level] = [];
+                                internals.ships[level].push(code);
+                                internals.names.set(code, prs.name.replace(/_/g, " "));
+                                internals.levels.set(code, { level: prs.level, model: prs.model });
+                                if (prs.typespec.model !== code % 100) internals.models.set(code, prs.typespec.model);
+                                let cnxt = uAr(Array.isArray(next) ? next : []);
+                                if (cnxt.length > 0) internals.nexts.set(code, cnxt)
+                            } catch (e) {
+                                game.custom.ships = default_options;
+                                internals = game.custom.ships;
+                                break
+                            }
+                        }
+                    for (let i in internals.ships) {
+                        internals.ships[i] = uAr(internals.ships[i]).sort(function(a, b) {
+                            let al = internals.levels.get(a) || {},
+                                bl = internals.levels.get(b) || {};
+                            return getNum(al.model) - getNum(bl.model);
+                        })
+                    }
+                    shipSelect.html([...internals.names.entries()].sort(function(a, b) {
+                        let al = internals.levels.get(a[0]) || {},
+                            bl = internals.levels.get(b[0]) || {};
+                        let t = getNum(al.level) - getNum(bl.level);
+                        if (t != 0) return t;
                         return getNum(al.model) - getNum(bl.model);
-                    })
-                }
-                shipSelect.html([...internals.names.entries()].sort(function(a, b) {
-                    let al = internals.levels.get(a[0]) || {},
-                        bl = internals.levels.get(b[0]) || {};
-                    let t = getNum(al.level) - getNum(bl.level);
-                    if (t != 0) return t;
-                    return getNum(al.model) - getNum(bl.model);
-                }).map(name => {
-                    let levels = internals.levels.get(name[0]);
-                    return `<a id="${name[0]}" href="javascript:void 0" onclick="$('#ship-input').val('${name[1]}')">(L${getNum(levels.level)} - M${getNum(levels.model)}) ${name[1]}</a>`
-                }).join(""));
+                    }).map(name => {
+                        let levels = internals.levels.get(name[0]);
+                        return `<a id="${name[0]}" href="javascript:void 0" onclick="$('#ship-input').val('${name[1]}')">(L${getNum(levels.level)} - M${getNum(levels.model)}) ${name[1]}</a>`
+                    }).join(""));
 
-                filter();
+                    filter();
 
-                showResults("Ship tree successfully loaded.");
-            }).catch(function(e) { showError("Failed to get ship tree info") })
-        }
+                    showResults("Ship tree successfully loaded.");
+                    res();
+                }).catch(function(e) { showError("Failed to get ship tree info"); res(); })
+            }
+        });
     }
 
     let filter = function() {
@@ -175,6 +178,12 @@
 
     let findPath = function() {
         let ship_name = shipInput.val();
+        let newURL = new URL(location.href);
+        newURL.searchParams.set("query", ship_name || "");
+        newURL.searchParams.set("mod", $("#tree-select").val());
+
+        newURL = newURL.toString();
+        window.history.pushState({ path: newURL }, '', newURL);
         if (!ship_name || !mod_name) {
             if (!mod_name) showError("Please choose a ship tree to lookup");
             else showError("Please enter a ship to lookup")
@@ -241,7 +250,7 @@
 
     let shipChoose = $("#ship-choose");
 
-    for (let event of["click", "focus"]) {
+    for (let event of ["click", "focus"]) {
         $(document).on(event, "*", function(e) {
             e.stopPropagation();
             let display = ($.contains(shipChoose[0], e.target) && (e.type !== "click" || !$.contains(shipSelect[0], e.target))) ? "" : "none";
@@ -253,7 +262,7 @@
         });
     }
 
-    for (let event of["propertychange", "input"]) {
+    for (let event of ["propertychange", "input"]) {
         shipInput.on(event, filter);
     }
     shipChoose.on("keydown", focusControl);
@@ -262,4 +271,21 @@
         if (event.ctrlKey && event.keyCode == 13) findPath();
     });
     addToolPage(null, "1%", "1%", null);
+
+    let search = new URL(location.href).searchParams;
+
+    if (search.has("mod")) {
+        let mod_name = search.get("mod").toLowerCase();
+        let selector = $("#tree-select");
+        let available_values = [...selector[0].options].slice(1).map(e => e.value);
+        if (available_values.includes(mod_name)) {
+            selector.val(mod_name);
+            loadTree(mod_name).then(() => {
+                if (!search.has("query")) return;
+                let query = search.get("query");
+                $("#ship-input").val(query);
+                $("#lookup").click();
+            });
+        }
+    }
 })();
